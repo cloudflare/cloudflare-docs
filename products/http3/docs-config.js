48726078e1933aa57139f84f60fe715789940f37
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-10-17 17:00:54 -0400

    improve http/3 title, add external links

--- products/http3/docs-config.js
+++ products/http3/docs-config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  product: "HTTP3",
+  product: "HTTP/3",
   pathPrefix: "/http3",
   productLogoPathD: "M8 8v32h32V26H22V8zm18-2h16v16H26z",
   contentRepo: "cloudflare/cloudflare-docs",
   contentRepoFolder: "products/http3",
   externalLinks: [
+    {
+      title: "HTTP/3 posts on the Cloudflare blog",
+      url: "https://blog.cloudflare.com/tag/http3/"
+    },
+    {
+      title: "HTTP/3 vs. HTTP/3 performance",
+      url: "https://blog.cloudflare.com/http-3-vs-http-2/"
+    }
     {
       title: "Cloudflare homepage",
       url: "https://cloudflare.com"
@@ -15,8 +23,8 @@ module.exports = {
     apiKey: "",
   },
   siteMetadata: {
-    title: "Cloudflare HTTP3 docs",
-    description: "HTTP3 is the newest version of the Hypertext Transfer Protocol",
+    title: "Cloudflare HTTP/3 docs",
+    description: "HTTP/3 is the newest version of the Hypertext Transfer Protocol",
     author: "@cloudflare",
     url: "https://developers.cloudflare.com/http3",
     image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
