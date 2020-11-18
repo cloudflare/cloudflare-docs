87ca616bb9cbe232833e90ddaa7ad0d06983d5b1
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-11-18 12:33:50 -0500

    kickstart cloudflare one site

---
+++ products/cloudflare-one/README.md
@@ -0,0 +1,5 @@
+# Cloudflare One docs
+
+[View docs →](https://developers.cloudflare.com/cloudflare-docs)
+
+[Read contributor guidelines →](https://developers.cloudflare.com/docs-engine/contributing/content-framework)
---
+++ products/cloudflare-one/docs-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  product: "Cloudflare for Teams",
+  pathPrefix: "/cloudflare-one",
+  productLogoPathD: "M8 8v32h32V26H22V8zm18-2h16v16H26z",
+  contentRepo: "cloudflare/cloudflare-docs",
+  contentRepoFolder: "products/cloudflare-one",
+  externalLinks: [
+    {
+      title: "Blog: Introducing Cloudflare One",
+      url: "https://blog.cloudflare.com/introducing-cloudflare-one/"
+    },
+    {
+      title: "Cloudflare homepage",
+      url: "https://cloudflare.com"
+    }
+  ],
+  search: {
+    indexName: "",
+    apiKey: "",
+  },
+  siteMetadata: {
+    title: "Cloudflare One docs",
+    description: "Cloudflare One™ is the culmination of engineering and technical development guided by conversations with thousands of customers about the future of the corporate network. It provides secure, fast, reliable, cost-effective network services, integrated with leading identity management and endpoint security providers.",
+    author: "@cloudflare",
+    url: "https://developers.cloudflare.com/cloudflare-one",
+    image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
+  }
+}
