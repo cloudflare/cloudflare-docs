238ba2c69110835c95c157548676e4dc0276d55e
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2020-11-24 12:52:56 -0500

    developers site design cleanup

--- developers.cloudflare.com/src/components/site-header.js
+++ developers.cloudflare.com/src/components/site-header.js
@@ -9,41 +9,45 @@ import ThemeToggle from "cloudflare-docs-engine/src/components/theme-toggle"
 import "../css/components/site-header.css"
 
 const SiteHeader = () => (
-  <div className="SiteHeader">
-    <div className="SiteHeader--logo-section">
-      <Link className="SiteHeader--cloudflare-logo-link Link Link-without-underline" to="/">
-        <DocsNavLogoLockup
-          logo={<CloudflareLogo/>}
-          text={(
-            <>
-              <span data-text="Cloudflare">Cloudflare</span>
-              <span>&nbsp;</span>
-              <span data-text="Docs">Developers</span>
-            </>
-          )}
-        />
-      </Link>
-    </div>
+  <>
+    <div className="SiteHeader">
+      <div className="SiteHeader--logo-section">
+        <Link className="SiteHeader--cloudflare-logo-link Link Link-without-underline" to="/">
+          <DocsNavLogoLockup
+            logo={<CloudflareLogo/>}
+            text={(
+              <>
+                <span data-text="Cloudflare">Cloudflare</span>
+                <span>&nbsp;</span>
+                <span data-text="Docs">Developers</span>
+              </>
+            )}
+          />
+        </Link>
+      </div>
 
-    <div className="SiteHeader--nav">
-      <Link className="SiteHeader--nav-link" to="/docs">
-        <span>Docs</span>
-      </Link>
-      <Link className="SiteHeader--nav-link" to="/sponsorships">
-        <span>Sponsorships</span>
-      </Link>
-      <a className="SiteHeader--nav-link" href="https://cloudflare.github.io">
-        <span>Open Source</span>
-      </a>
-      <a className="SiteHeader--nav-link" href="https://blog.cloudflare.com/serverlist/">
-        <span>Newsletter</span>
-      </a>
-    </div>
+      <div className="SiteHeader--nav">
+        <Link className="SiteHeader--nav-link" to="/docs">
+          <span>Docs</span>
+        </Link>
+        <Link className="SiteHeader--nav-link" to="/sponsorships">
+          <span>Sponsorships</span>
+        </Link>
+        <a className="SiteHeader--nav-link" href="https://cloudflare.github.io">
+          <span>Open Source</span>
+        </a>
+        <a className="SiteHeader--nav-link" href="https://blog.cloudflare.com/serverlist/">
+          <span>Newsletter</span>
+        </a>
+      </div>
 
-    <div className="SiteHeader--theme-toggle">
-      <ThemeToggle/>
+      <div className="SiteHeader--theme-toggle">
+        <ThemeToggle/>
+      </div>
     </div>
-  </div>
+    <div className="SiteHeader---shadow"></div>
+    <div className="SiteHeader---shadow-cover"></div>
+  </>
 )
 
 export default SiteHeader
