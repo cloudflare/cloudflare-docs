af24b60d5196c2b2385019d563a01467ea35f201
TownLake <srhea@cloudflare.com>
2020-12-29 11:01:05 +0000

    updates

--- products/cloudflare-one/src/content/components/content-table.js
+++ products/cloudflare-one/src/content/components/content-table.js
@@ -10,7 +10,7 @@ const getPageTitle = ({ frontmatter, headings }) => {
 
 const tenDaysInMS = 10 * 24 * 60 * 60 * 1000;
 
-const ContentTable = ({ path }) => {
+const DocsTutorials = () => {
   const query = useStaticQuery(graphql`
     query {
       allMdx {
@@ -34,34 +34,32 @@ const ContentTable = ({ path }) => {
         }
       }
     }
-  `);
+  `)
 
-  const regex = new RegExp(`^\/${path}\/.+`);
-
-  const pages = query.allMdx.edges
+  const tutorials = query.allMdx.edges
     .map(({ node }) => node)
-    .filter((page) => page.fields.slug.match(regex))
-    .map((page) => ({
+    .filter(page => page.fields.slug.match(/^\/tutorials\/.+/))
+    .map(page => ({
       title: getPageTitle(page),
       url: page.fields.slug,
       updated: page.frontmatter.updated,
       category: page.frontmatter.category,
       wordCount: page.wordCount.words,
-      new: +new Date() - +new Date(page.frontmatter.updated) < tenDaysInMS,
+      new: (+new Date() - +new Date(page.frontmatter.updated)) < tenDaysInMS
     }))
-    .sort((a, b) => +new Date(b.updated) - +new Date(a.updated));
+    .sort((a, b) => +new Date(b.updated) - +new Date(a.updated))
 
-  let i = 0;
-  let longestWordCount = 0;
+  let i = 0
+  let longestWordCount = 0
 
-  for (i = 0; i < pages.length; i += 1) {
-    if (pages[i].wordCount > longestWordCount) {
-      longestWordCount = pages[i].wordCount;
+  for (i = 0; i < tutorials.length; i += 1) {
+    if (tutorials[i].wordCount > longestWordCount) {
+      longestWordCount = tutorials[i].wordCount
     }
   }
 
-  for (i = 0; i < pages.length; i += 1) {
-    pages[i].length = (pages[i].wordCount / longestWordCount) * 100 + "%";
+  for (i = 0; i < tutorials.length; i += 1) {
+    tutorials[i].length = ((tutorials[i].wordCount / longestWordCount) * 100) + "%"
   }
 
   return (
@@ -84,48 +82,31 @@ const ContentTable = ({ path }) => {
       </div>
 
       <div className="DocsTutorials--body">
-        {pages.map((page) => (
-          <div
-            key={page.url}
-            className={
-              "DocsTutorials--row" +
-              (page.new ? " DocsTutorials--row-is-new" : "")
-            }
-          >
+        {tutorials.map(tutorial => (
+          <div key={tutorial.url} className={"DocsTutorials--row" + (tutorial.new ? " DocsTutorials--row-is-new" : "")}>
             <div className="DocsTutorials--column" data-column="name">
-              <Link className="DocsTutorials--link" to={page.url}>
-                {page.title}
+              <Link className="DocsTutorials--link" to={tutorial.url}>
+                {tutorial.title}
               </Link>
             </div>
             <div className="DocsTutorials--column" data-column="updated">
-              <TimeAgo
-                date={page.updated}
-                formatter={(value, unit) => (
-                  <React.Fragment>
-                    {value} {unit}
-                    {value > 1 ? "s" : ""}
-                    <span className="DocsTutorials--ago-text"> ago</span>
-                  </React.Fragment>
-                )}
-                minPeriod={60}
-              />
+              <TimeAgo date={tutorial.updated} formatter={(value, unit) => (
+                <React.Fragment>
+                  {value} {unit}{value > 1 ? "s" : ""}<span className="DocsTutorials--ago-text"> ago</span>
+                </React.Fragment>
+              )} minPeriod={60}/>
             </div>
-            <div className="DocsTutorials--column" data-column="category">
-              {page.category}
-            </div>
+            <div className="DocsTutorials--column" data-column="category">{tutorial.category}</div>
             <div className="DocsTutorials--column" data-column="length">
               <div className="DocsTutorials--length-bar">
-                <div
-                  className="DocsTutorials--length-bar-inner"
-                  style={{ width: page.length }}
-                ></div>
+                <div className="DocsTutorials--length-bar-inner" style={{width: tutorial.length}}></div>
               </div>
             </div>
           </div>
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ContentTable;
+export default DocsTutorials
