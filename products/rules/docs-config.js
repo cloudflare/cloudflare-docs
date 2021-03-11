682daa3ede19bdf8d1f6787cb762c613518e6b10
Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
2021-03-11 14:28:22 +0000

    Define temporary tile icon

--- products/rules/docs-config.js
+++ products/rules/docs-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   product: "Rules",
   pathPrefix: "/rules",
-  productIconKey: "firewall",
+//  productIconKey: "rules",
+  productLogoPathD: "m9.2446 4.7416-1.5 1.5v35.85l1.5 1.5h30l1.5-1.5v-35.85l-1.5-1.5zm1.5 3h27v32.85h-27zm3.3897 3.5332v3h10.005v-3zm0 6.2696v3h20.139v-3zm0 6.2696v3h20.139v-3z",
   contentRepo: "cloudflare/cloudflare-docs",
   contentRepoFolder: "products/rules",
   externalLinks: [
