d06abb21c09505764aa6a1b2020736e80e1b96f3
Kody Jackson <kody@cloudflare.com>
2021-03-19 12:33:51 -0500

    Added initial files

---
+++ products/page-shield/docs-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  product: "Page Shield",
+  pathPrefix: "/page-shield",
+  productIconKey: "page-shield",
+  contentRepo: "cloudflare/cloudflare-docs",
+  contentRepoFolder: "products/page-shield",
+  externalLinks: [
+    {
+      title: "Cloudflare homepage",
+      url: "https://cloudflare.com"
+    }
+  ],
+  search: { },
+  siteMetadata: {
+    title: "Cloudflare Page Shield docs",
+    description: "Create rules that examine incoming HTTP traffic against a set of powerful filters to block, challenge, log, or allow matching requests.",
+    author: "@cloudflare",
+    url: "https://developers.cloudflare.com/page-shield",
+    image: ""
+  }
+}
