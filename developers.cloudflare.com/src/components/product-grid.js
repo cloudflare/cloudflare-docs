e3e9ef24e42188829c47b7d53fc7562b0e581e0f
crwaters16 <78226508+crwaters16@users.noreply.github.com>
2021-04-20 15:41:48 -0500

    [Content Strategy] Add Cloudflare Fundamentals section to developer docs (#954)
    * Renaming Internet section to Cloudflare Fundamentals to repurpose section
    
    * Rearranging existing Internet docs
    
    * troubleshooting indexing error p2
    
    * updating gatsby-config.js to remove internet and add fundamentals
    
    * Update build.yml
    
    * updating additional instances of internet to fundamentals
    
    * Update deploy.yml
    
    * implementing edits to address pedro's comments
    
    * Update .github/workflows/build.yml
    
    Co-authored-by: John Fawcett <jfawcett@cloudflare.com>
    
    * Update .github/workflows/build.yml
    
    Co-authored-by: John Fawcett <jfawcett@cloudflare.com>
    
    * updating brand assets version
    
    * merging with production
    
    * fixing product grid wrap
    
    * updating gatsby and brand assets
    
    Co-authored-by: John Fawcett <jfawcett@cloudflare.com>

--- developers.cloudflare.com/src/components/product-grid.js
+++ developers.cloudflare.com/src/components/product-grid.js
@@ -14,7 +14,7 @@ $["distributed-web"]      = require("@cloudflare/cloudflare-brand-assets/resourc
 $["firewall"]             = require("@cloudflare/cloudflare-brand-assets/resources/product-icons/firewall.js").pathD
 $["http3"]                = require("@cloudflare/cloudflare-brand-assets/resources/product-icons/http3.js").pathD
 $["images"]               = require("@cloudflare/cloudflare-brand-assets/resources/product-icons/images.js").pathD
-$["internet"]             = require("@cloudflare/cloudflare-brand-assets/resources/product-icons/internet.js").pathD
+$["fundamentals"]         = require("@cloudflare/cloudflare-brand-assets/resources/product-icons/fundamentals.js").pathD
 $["load-balancing"]       = require("@cloudflare/cloudflare-brand-assets/resources/product-icons/load-balancing.js").pathD
 $["logs"]                 = require("@cloudflare/cloudflare-brand-assets/resources/product-icons/logs.js").pathD
 $["magic-transit"]        = require("@cloudflare/cloudflare-brand-assets/resources/product-icons/magic-transit.js").pathD
@@ -100,9 +100,10 @@ const products = [
     icon: "images",
   },
   {
-    title: "Internet",
-    path: "internet",
-    icon: "internet",
+    title: "Cloudflare Fundamentals",
+    path: "fundamentals",
+    icon: "fundamentals",
+    wrap: true
   },
   {
     title: "Load Balancing",
