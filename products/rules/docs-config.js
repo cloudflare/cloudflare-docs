9f338f82f2579f851eef86d82e12c7c57bcbd591
Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
2021-04-21 18:45:56 +0100

    Use Rules icon from cloudflare-brand-assets (#1186)

--- products/rules/docs-config.js
+++ products/rules/docs-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   product: "Rules",
   pathPrefix: "/rules",
-//  productIconKey: "rules",
-  productLogoPathD: "m9.2446 4.7416-1.5 1.5v35.85l1.5 1.5h30l1.5-1.5v-35.85l-1.5-1.5zm1.5 3h27v32.85h-27zm3.3897 3.5332v3h10.005v-3zm0 6.2696v3h20.139v-3zm0 6.2696v3h20.139v-3z",
+  productIconKey: "rules",
   contentRepo: "cloudflare/cloudflare-docs",
   contentRepoFolder: "products/rules",
   externalLinks: [
