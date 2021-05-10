e4b79d97afddc99c93ce1bf5fa287aa722094ec0
Denise Peña <75506267+dcpena@users.noreply.github.com>
2021-05-10 13:26:10 -0500

    Adding APO docs (#1293)
    * required files and folder setup
    
    * adding docs for APO setup
    
    * Adding the rest of the APO content
    
    * implementing feedback
    
    * implementing feedback and some reorg
    
    * more feedback updates
    
    * Implementing feedback
    
    * Resolving conflict
    
    * Added sitemap for APO and existing docs
    
    * Fixed spelling in sitemap
    
    Co-authored-by: Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
    
    * Fixed typo in Test URL
    
    Co-authored-by: Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
    
    * Unintentionally added. Removing
    
    Co-authored-by: Pedro Sousa <680496+pedrosousa@users.noreply.github.com>

---
+++ products/automatic-platform-optimization/docs-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  product: "Automatic Platform Optimization",
+  pathPrefix: "/automatic-platform-optimization",
+  productIconKey: "automatic-platform-optimization",
+  contentRepo: "cloudflare/cloudflare-docs",
+  contentRepoFolder: "products/automatic-platform-optimization",
+  externalLinks: [
+    {
+      title: "Cloudflare homepage",
+      url: "https://cloudflare.com"
+    },
+    {
+      title: "Blog: Introducing APO",
+      url: "https://blog.cloudflare.com/automatic-platform-optimizations-starting-with-wordpress/"
+    },
+    {
+      title: "Blog: APO Post-launch report",
+      url: "https://blog.cloudflare.com/apo-post-launch-report/"
+    }
+  ],
+  search: {
+    indexName: "developers-cloudflare",
+    apiKey: "b23088ab4d346409f9d3ece6606344c3",
+    algoliaOptions: { 'facetFilters': '["project:automatic-platform-optimization"]'}
+  },
+  siteMetadata: {
+    title: "Cloudflare Automatic Platform Optimization",
+    description: "With Automatic Platform Optimization, Cloudflare serves your entire site from our edge network, ensuring customers see improved performance when visiting your site.",
+    author: "@cloudflare",
+    url: "https://developers.cloudflare.com/automatic-platform-optimization",
+    image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
+  }
+}
