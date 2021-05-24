4ebcdb1466637eb37131dc9950b9aaf201bdb304
marciocloudflare <83226960+marciocloudflare@users.noreply.github.com>
2021-05-24 14:49:54 +0100

    Marcio/migrate railgun docs to dev docs (#1375)
    * Added railgun folder with starting docs
    
    * testing creating docs and folders
    
    * adding files
    
    * docs
    
    * railgun docs
    
    * railgun docs
    
    * finishing docs
    
    * internal links changed
    
    * type and links
    
    * semi final migration
    
    * changed page order
    
    * miscellaneous
    
    * cleaned table introduction
    
    * space with units and hyperlink
    
    * final touches
    
    * type of page
    
    * Update products/railgun/README.md
    
    Co-authored-by: Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
    
    * sentence case and alice suggestions
    
    * corrected spelling connections
    
    * removed misspelled docs and corrected url
    
    * ignore spelling
    
    * changed from txt to sh
    
    * deleted space between bolds
    
    * addes json to type of block code
    
    * added collapsible DIVs
    
    * updated the build files
    
    * Update products/railgun/src/content/user-guide/client-api/connections-enabling-disabling.md
    
    Co-authored-by: abracchi-tw <63585571+abracchi-tw@users.noreply.github.com>
    
    * Update products/railgun/src/content/user-guide/client-api/creation-activation-deletion.md
    
    Co-authored-by: abracchi-tw <63585571+abracchi-tw@users.noreply.github.com>
    
    * Update products/railgun/src/content/user-guide/client-api/listing-and-viewing-details.md
    
    Co-authored-by: abracchi-tw <63585571+abracchi-tw@users.noreply.github.com>
    
    * Update products/railgun/src/content/user-guide/client-api/listing-and-viewing-details.md
    
    Co-authored-by: abracchi-tw <63585571+abracchi-tw@users.noreply.github.com>
    
    * removed vscode settings
    
    * changed name of file and title
    
    * changed links to reflect new doc naming scheme
    
    * removed hiphen from end users
    
    * changed title to overview
    
    * removed irrelevant information
    
    * extra space after --- and period mark on first
    2 sentences
    
    * changed step numbers to convention 1.
    
    * space after h1
    
    * changed type from overview to document
    
    * moved 64-bit info to package repository
    
    * removed type document from default documents
    
    * addes sh to code type
    
    * fixed raingul typo
    
    * corrected to common issues and added
    link to section
    
    * added sh to code blocks
    
    * changed Test formatting
    
    * replaced % with $ and added sh to code block
    
    * switched eg for example
    
    * removed title when its the same as h1
    
    * added period to sentence
    
    * added period to sentences
    
    * json in code block
    
    * added json and sh to code blocks
    
    * added txt to code block
    
    * corrected to paragraph from numered list
    
    * moved changelog and licence to below user guide
    
    * changed from href to path "railgun"
    
    * added railgun to products
    
    * added railgun row to the table
    
    * emtpy string for now on search config
    
    * clarified port2408.net is an example
    
    * added sh to block code and space after paragraph
    
    * added sh to code block
    
    * broke-up installation.md into several
    smaller documents to make reading easier
    
    Co-authored-by: Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
    Co-authored-by: abracchi-tw <63585571+abracchi-tw@users.noreply.github.com>

---
+++ products/railgun/docs-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    product: "Railgun",
+    pathPrefix: "/railgun",
+    productIconKey: "railgun",
+    contentRepo: "cloudflare/cloudflare-docs",
+    contentRepoFolder: "products/railgun",
+    externalLinks: [
+      {
+        title: "Cloudflare homepage",
+        url: "https://cloudflare.com"
+      }
+    ],
+    search: {
+      indexName: "",
+      apiKey: "",
+      algoliaOptions: { 'facetFilters': ''}
+    },
+    siteMetadata: {
+      title: "Railgun Documentation",
+      description: "Improve loading times by caching dynamically generated or personalized web pages, dramatically reducing bandwidth usage and improving download times.",
+      author: "@cloudflare",
+      url: "https://developers.cloudflare.com/railgun",
+      image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
+    }
+  }
