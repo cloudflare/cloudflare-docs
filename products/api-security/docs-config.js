cabc974cc9084c5e2823aa444c5a8dd97fd838df
kodster28 <kody@cloudflare.com>
2021-05-25 11:26:20 -0500

    Initial api security docs.  (#1408)
    * Initial api security docs. PM is okay with temporarily using the same product icon as page shield
    
    * Updating product grid
    
    * Update build.yml
    
    * Update deploy.yml
    
    * Updated README

---
+++ products/api-security/docs-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    product: "API Security",
+    pathPrefix: "/api-security",
+    productIconKey: "page-shield",
+    contentRepo: "cloudflare/cloudflare-docs",
+    contentRepoFolder: "products/api-security",
+  externalLinks: [
+    {
+      title: "Cloudflare homepage",
+      url: "https://cloudflare.com"
+    }
+  ],
+  search: {
+    indexName: "",
+    apiKey: "",
+    algoliaOptions: { 'facetFilters': ''}
+  },
+  siteMetadata: {
+    title: "Cloudflare API Security docs",
+    description: "Protect your APIs from simple and sophisticated attacks using Cloudflare API Security products.",
+    author: "@cloudflare",
+    url: "https://developers.cloudflare.com/api-security",
+    image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
+  }
+}
