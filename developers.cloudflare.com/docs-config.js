6fbd077e78e14f2a4906b144e1732d0e498bcad9
Ganchimeg Ganzorig <ganchimeg10@gmail.com>
2021-06-18 11:22:18 -0500

    edited site metatag url for all products without a custom url (#1592)
    Co-authored-by: ganchimeg <ganchimeg@cloudflare.com>

--- developers.cloudflare.com/docs-config.js
+++ developers.cloudflare.com/docs-config.js
@@ -18,6 +18,6 @@ module.exports = {
     description: "Browse the latest developer documentation including API reference, articles, and sample code.",
     author: "@cloudflare",
     url: "https://developers.cloudflare.com",
-    image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
+    image: "https://www.cloudflare.com/img/cf-twitter-card.png"
   }
 }
--- products/railgun/docs-config.js
+++ products/railgun/docs-config.js
@@ -20,6 +20,6 @@ module.exports = {
       description: "Improve loading times by caching dynamically generated or personalized web pages, dramatically reducing bandwidth usage and improving download times.",
       author: "@cloudflare",
       url: "https://developers.cloudflare.com/railgun",
-      image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
+      image: "https://www.cloudflare.com/img/cf-twitter-card.png"
     }
   }
