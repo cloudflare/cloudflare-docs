bf2fbf481378588d7785564ee4ba3596353fc907
marciocloudflare <83226960+marciocloudflare@users.noreply.github.com>
2021-06-22 14:20:26 +0100

    updated title and description of (#1630)
    Fundamentals metadata

--- products/fundamentals/docs-config.js
+++ products/fundamentals/docs-config.js
@@ -20,8 +20,8 @@ module.exports = {
     algoliaOptions: { 'facetFilters': '["project:fundamentals"]'}
   },
   siteMetadata: {
-    title: "The Life of a Cloudflare Request docs",
-    description: "Undersea cables, peering agreements and TLS handshakes. Follow the life of a request across the internet and see how it all works.",
+    title: "Cloudflare Fundamentals",
+    description: "Cloudflare Fundamentals provides information about features that span Cloudflare products.",
     author: "@cloudflare",
     url: "https://developers.cloudflare.com/fundamentals",
     image: "https://www.cloudflare.com/img/cf-twitter-card.png"
