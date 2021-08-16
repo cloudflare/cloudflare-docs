847110fc96e1cc84e4e5728aff18d87e1d9f83c5
Steven Raden <sraden@cloudflare.com>
2021-08-16 13:58:57 -0500

    added full SVG files for the eventual new tooling. Not in a working state

--- developers.cloudflare.com/src/components/product-grid.js
+++ developers.cloudflare.com/src/components/product-grid.js
@@ -3,45 +3,45 @@ import React from "react"
 import "../css/components/product-grid.css"
 
 const $ = {}
-$["1.1.1.1"]              = require("../../../products/1.1.1.1/src/content/icons/1111.js").pathD
-$["analytics"]            = require("../../../products/analytics/src/content/icons/analytics.js").pathD
-$["api"]                  = require("../../../products/api/src/content/icons/api.js").pathD
-$["apps"]                  = require("../../../products/apps.js").pathD
-$["api-security"]         = require("../../../products/api-security/src/content/icons/api-security.js").pathD
-$["automatic-platform-optimization"]   = require("../../../products/automatic-platform-optimization/src/content/icons/automatic-platform-optimization.js").pathD
-$["bots"]                 = require("../../../products/bots/src/content/icons/bots.js").pathD
-$["byoip"]                = require("../../../products/byoip/src/content/icons/byoip.js").pathD
-$["cache"]                = require("../../../products/cache/src/content/icons/cache.js").pathD
-$["cloudflare-for-teams"] = require("../../../products/cloudflare-one/src/content/icons/cloudflare-teams.js").pathD
-$["cloudflare-one"]       = require("../../../products/cloudflare-one/src/content/icons/cloudflare-teams.js").pathD
-$["distributed-web"]      = require("../../../products/distributed-web/src/content/icons/distributed-web.js").pathD
-$["firewall"]             = require("../../../products/firewall/src/content/icons/firewall.js").pathD
-$["http3"]                = require("../../../products/http3/src/content/icons/http3.js").pathD
-$["images"]               = require("../../../products/images/src/content/icons/images.js").pathD
-$["fundamentals"]         = require("../../../products/fundamentals/src/content/icons/cloudflare-fundamentals.js").pathD
-$["load-balancing"]       = require("../../../products/load-balancing/src/content/icons/load-balancing.js").pathD
-$["logs"]                 = require("../../../products/logs/src/content/icons/logs.js").pathD
-$["magic-transit"]        = require("../../../products/magic-transit/src/content/icons/magic-transit.js").pathD
-$["magic-firewall"]       = require("@cloudflare/cloudflare-brand-assets/resources/product-icons/firewall.js").pathD
-$["magic-wan"]            = require("../../../products/magic-wan/src/content/icons/magic-wan.js").pathD
-$["network-interconnect"] = require("../../../products/network-interconnect/src/content/icons/network-interconnect.js").pathD
-$["pages"]                = require("../../../products/pages/src/content/icons/pages.js").pathD
-$["page-shield"]          = require("../../../products/page-shield/src/content/icons/page-shield.js").pathD
-$["partners"]             = require("../../../products/partners/src/content/icons/partners.js").pathD
-$["railgun"]              = require("../../../products/railgun/src/content/icons/railgun.js").pathD
-$["randomness-beacon"]    = require("../../../products/randomness-beacon/src/content/icons/randomness-beacon.js").pathD
-$["registrar"]            = require("../../../products/registrar/src/content/icons/registrar.js").pathD
-$["rules"]                = require("../../../products/rules/src/content/icons/rules.js").pathD
-$["spectrum"]             = require("../../../products/spectrum/src/content/icons/spectrum.js").pathD
-$["ssl"]                  = require("../../../products/ssl/src/content/icons/ssl.js").pathD
-$["stream"]               = require("../../../products/stream/src/content/icons/stream.js").pathD
-$["tenant"]               = require("../../../products/tenant/src/content/icons/tenant.js").pathD
-$["terraform"]            = require("../../../products/terraform/src/content/icons/terraform.js").pathD
-$["time-services"]        = require("../../../products/time-services/src/content/icons/time-services.js").pathD
-$["waf"]                  = require("../../../products/waf/src/content/icons/waf.js").pathD
-$["waiting-room"]         = require("../../../products/waiting-room/src/content/icons/waiting-room.js").pathD
-$["warp-client"]          = require("../../../products/warp-client/src/content/icons/warp-client.js").pathD
-$["workers"]              = require("../../../products/workers/src/content/icons/workers.js").pathD
+$["1.1.1.1"]              = require("../../../products/1.1.1.1/src/content/icons/1111.svg")
+$["analytics"]            = require("../../../products/analytics/src/content/icons/analytics.svg")
+$["api"]                  = require("../../../products/api/src/content/icons/api.svg")
+$["apps"]                 = require("../../../products/apps.svg")
+// $["api-security"]         = require("../../../products/api-security/src/content/icons/api-security.svg")
+$["automatic-platform-optimization"]   = require("../../../products/automatic-platform-optimization/src/content/icons/automatic-platform-optimization.svg")
+$["bots"]                 = require("../../../products/bots/src/content/icons/bots.svg")
+// $["byoip"]                = require("../../../products/byoip/src/content/icons/byoip.svg")
+$["cache"]                = require("../../../products/cache/src/content/icons/cache.svg")
+$["cloudflare-for-teams"] = require("../../../products/cloudflare-one/src/content/icons/cloudflare-teams.svg")
+$["cloudflare-one"]       = require("../../../products/cloudflare-one/src/content/icons/cloudflare-teams.svg")
+$["distributed-web"]      = require("../../../products/distributed-web/src/content/icons/distributed-web.svg")
+// $["firewall"]             = require("../../../products/firewall/src/content/icons/firewall.svg")
+$["http3"]                = require("../../../products/http3/src/content/icons/http3.svg")
+// $["images"]               = require("../../../products/images/src/content/icons/images.svg")
+$["fundamentals"]         = require("../../../products/fundamentals/src/content/icons/fundamentals.svg")
+$["load-balancing"]       = require("../../../products/load-balancing/src/content/icons/load-balancing.svg")
+$["logs"]                 = require("../../../products/logs/src/content/icons/logs.svg")
+$["magic-transit"]        = require("../../../products/magic-transit/src/content/icons/magic-transit.svg")
+$["magic-firewall"]       = require("@cloudflare/cloudflare-brand-assets/resources/product-icons/firewall.js")
+$["magic-wan"]            = require("../../../products/magic-wan/src/content/icons/magic-wan.svg")
+$["network-interconnect"] = require("../../../products/network-interconnect/src/content/icons/network-interconnect.svg")
+$["pages"]                = require("../../../products/pages/src/content/icons/pages.svg")
+$["page-shield"]          = require("../../../products/page-shield/src/content/icons/page-shield.svg")
+$["partners"]             = require("../../../products/partners/src/content/icons/partners.svg")
+$["railgun"]              = require("../../../products/railgun/src/content/icons/railgun.svg")
+$["randomness-beacon"]    = require("../../../products/randomness-beacon/src/content/icons/randomness-beacon.svg")
+$["registrar"]            = require("../../../products/registrar/src/content/icons/registrar.svg")
+$["rules"]                = require("../../../products/rules/src/content/icons/rules.svg")
+$["spectrum"]             = require("../../../products/spectrum/src/content/icons/spectrum.svg")
+$["ssl"]                  = require("../../../products/ssl/src/content/icons/ssl.svg")
+$["stream"]               = require("../../../products/stream/src/content/icons/stream.svg")
+$["tenant"]               = require("../../../products/tenant/src/content/icons/tenant.svg")
+$["terraform"]            = require("../../../products/terraform/src/content/icons/terraform.svg")
+$["time-services"]        = require("../../../products/time-services/src/content/icons/time-services.svg")
+$["waf"]                  = require("../../../products/waf/src/content/icons/waf.svg")
+// $["waiting-room"]         = require("../../../products/waiting-room/src/content/icons/waiting-room.svg")
+$["warp-client"]          = require("../../../products/warp-client/src/content/icons/warp-client.svg")
+$["workers"]              = require("../../../products/workers/src/content/icons/workers.svg")
 
 const products = [
   {
@@ -249,7 +249,9 @@ const products = [
 
 const ProductGridLink = ({ product }) => (
   <a className="ProductGrid--link" data-wrap-title={product.wrap} href={product.href || `https://developers.cloudflare.com/${product.path}`}>
-    <svg viewBox="0 0 48 48"><path d={$[product.icon]}/></svg>
+    {/* <img src={$[product.icon]} alt={$[product.title] + " icon"} /> */}
+    {/* <svg viewBox="0 0 48 48"><path d={$[product.icon]}/></svg> */}
+    {/* Need new image format inserted here */}
     <span>{product.title}</span>
   </a>
 )
