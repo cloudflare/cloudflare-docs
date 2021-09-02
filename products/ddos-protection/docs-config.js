247ab2a814863f2efd76edd4f55467d5eae030e3
Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
2021-09-02 12:52:00 +0100

    Update docs-config.js

--- products/ddos-protection/docs-config.js
+++ products/ddos-protection/docs-config.js
@@ -4,7 +4,6 @@ module.exports = {
   productLogoPathD: require('./src/content/icons/ddos-protection').pathD,
   contentRepo: "cloudflare/cloudflare-docs",
   contentRepoFolder: "products/ddos-protection",
-//  logoSVGContent: fs.readFileSync(`../src/content/icons/ddos-protection.svg`, 'utf8'),
   externalLinks: [
     {
       title: "Cloudflare DDoS Protection homepage",
