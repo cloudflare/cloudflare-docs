d32e3b72480d2c99db3acbda6bedf595d4c81f84
Ganchimeg Ganzorig <ganchimeg10@gmail.com>
2021-09-13 10:52:05 -0500

    This PR will create preview links for any pull request with changes o… (#2082)
    * This PR will create preview links for any pull request with changes on the PR
    
    * minor changes from comments
    
    * Update .github/workflows/preview.yml
    
    * Update .github/workflows/preview.yml
    
    * Update products/rules/src/content/transform/index.md
    
    Co-authored-by: John Fawcett <jfawcett@cloudflare.com>
    
    Co-authored-by: ganchimeg <ganchimeg@cloudflare.com>
    Co-authored-by: John Fawcett <jfawcett@cloudflare.com>

---
+++ get-products-from-file-paths.js
@@ -0,0 +1,30 @@
+let data = "";
+
+process.stdin.on("data", (chunk) => {
+  data += chunk;
+});
+
+process.stdin.on("end", () => {
+  let changedProducts = new Set();
+  let transformedProducts = data.toString().trim().split(" ");
+  transformedProducts.forEach((product) => {
+    let splittedStr = product.split("/");
+    const productName = splittedStr[1];
+
+    if (!changedProducts.has(splittedStr[0] === "developers.cloudflare.com")) {
+      changedProducts.add("developers.cloudflare.com");
+    }
+
+    if (!changedProducts.has(productName)) {
+      changedProducts.add(productName);
+    }
+  });
+
+  let chandedProductsArr = [...changedProducts];
+
+  chandedProductsArr.forEach((prod) => {
+    process.stdout.write(`${prod}\n`);
+  });
+
+  process.exit(0);
+});
