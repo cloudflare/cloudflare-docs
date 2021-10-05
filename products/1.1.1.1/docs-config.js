3e6bde172c227082abc81e5abc308d4d12746aed
ganchimeg <ganchimeg@cloudflare.com>
2021-10-05 17:23:29 -0500

    reverting part 1 of icon update

--- products/1.1.1.1/docs-config.js
+++ products/1.1.1.1/docs-config.js
@@ -1,37 +1,29 @@
-const fs = require("fs");
-const productIconKey = "1.1.1.1";
-const path = require("path");
-
 module.exports = {
   product: "1.1.1.1",
   pathPrefix: "/1.1.1.1",
-  productIconKey,
+  productIconKey: "1.1.1.1",
   contentRepo: "cloudflare/cloudflare-docs",
   contentRepoFolder: "products/1.1.1.1",
-  logoSVGContent: fs.readFileSync(
-    path.join(__dirname, `src/content/icons/${productIconKey}.svg`),
-    "utf8"
-  ),
   externalLinks: [
     {
       title: "1.1.1.1 website",
-      url: "https://1.1.1.1",
+      url: "https://1.1.1.1"
     },
     {
       title: "Cloudflare homepage",
-      url: "https://cloudflare.com",
-    },
+      url: "https://cloudflare.com"
+    }
   ],
   search: {
     indexName: "developers-cloudflare",
     apiKey: "b23088ab4d346409f9d3ece6606344c3",
-    algoliaOptions: { facetFilters: '["project:1.1.1.1"]' },
+    algoliaOptions: { 'facetFilters': '["project:1.1.1.1"]'}
   },
   siteMetadata: {
     title: "1.1.1.1 docs",
     description: "A blazing fast DNS resolver built for private browsing.",
     author: "@cloudflare",
     url: "https://developers.cloudflare.com/1.1.1.1",
-    image: "https://www.cloudflare.com/img/cf-twitter-card.png",
-  },
-};
+    image: "https://www.cloudflare.com/img/cf-twitter-card.png"
+  }
+}
--- products/analytics/docs-config.js
+++ products/analytics/docs-config.js
@@ -1,34 +1,25 @@
-const fs = require("fs");
-const productIconKey = "analytics";
-const path = require("path");
-
 module.exports = {
   product: "Analytics",
   pathPrefix: "/analytics",
-  productIconKey,
+  productIconKey: "analytics",
   contentRepo: "cloudflare/cloudflare-docs",
   contentRepoFolder: "products/analytics",
-  logoSVGContent: fs.readFileSync(
-    path.join(__dirname, `src/content/icons/${productIconKey}.svg`),
-    "utf8"
-  ),
   externalLinks: [
     {
       title: "Cloudflare homepage",
-      url: "https://cloudflare.com",
-    },
+      url: "https://cloudflare.com"
+    }
   ],
   search: {
     indexName: "developers-cloudflare",
     apiKey: "b23088ab4d346409f9d3ece6606344c3",
-    algoliaOptions: { facetFilters: '["project:analytics"]' },
+    algoliaOptions: { 'facetFilters': '["project:analytics"]'}
   },
   siteMetadata: {
     title: "Cloudflare Analytics API docs",
-    description:
-      "With the GraphQL Analytics API, all of your performance, security, and reliability data is available from one endpoint. And you can select exactly what you need, from one metric for a domain to multiple metrics aggregated for your account.",
+    description: "With the GraphQL Analytics API, all of your performance, security, and reliability data is available from one endpoint. And you can select exactly what you need, from one metric for a domain to multiple metrics aggregated for your account.",
     author: "@cloudflare",
     url: "https://developers.cloudflare.com/analytics",
-    image: "https://www.cloudflare.com/img/cf-twitter-card.png",
-  },
-};
+    image: "https://www.cloudflare.com/img/cf-twitter-card.png"
+  }
+}
