41dc15be0bec255184abd08253aa36386dfe6f4d
Ganchimeg Ganzorig <ganchimeg10@gmail.com>
2021-10-05 16:22:04 -0500

    update product icons, implement new process to use icons (#2446)
    * update product icons, implement new process to use icons
    
    * update product icons, implement new process to use icons
    
    Co-authored-by: ganchimeg <ganchimeg@cloudflare.com>

--- products/railgun/docs-config.js
+++ products/railgun/docs-config.js
@@ -1,25 +1,34 @@
+const fs = require("fs");
+const productIconKey = "railgun";
+const path = require("path");
+
 module.exports = {
-    product: "Railgun",
-    pathPrefix: "/railgun",
-    productIconKey: "railgun",
-    contentRepo: "cloudflare/cloudflare-docs",
-    contentRepoFolder: "products/railgun",
-    externalLinks: [
-      {
-        title: "Cloudflare homepage",
-        url: "https://cloudflare.com"
-      }
-    ],
-    search: {
-      indexName: "developers-cloudflare",
-      apiKey: "b23088ab4d346409f9d3ece6606344c3",
-      algoliaOptions: { 'facetFilters': '["project:railgun"]'}
+  product: "Railgun",
+  pathPrefix: "/railgun",
+  productIconKey,
+  contentRepo: "cloudflare/cloudflare-docs",
+  contentRepoFolder: "products/railgun",
+  logoSVGContent: fs.readFileSync(
+    path.join(__dirname, `src/content/icons/${productIconKey}.svg`),
+    "utf8"
+  ),
+  externalLinks: [
+    {
+      title: "Cloudflare homepage",
+      url: "https://cloudflare.com",
     },
-    siteMetadata: {
-      title: "Railgun Documentation",
-      description: "Improve loading times by caching dynamically generated or personalized web pages, dramatically reducing bandwidth usage and improving download times.",
-      author: "@cloudflare",
-      url: "https://developers.cloudflare.com/railgun",
-      image: "https://www.cloudflare.com/img/cf-twitter-card.png"
-    }
-  }
+  ],
+  search: {
+    indexName: "developers-cloudflare",
+    apiKey: "b23088ab4d346409f9d3ece6606344c3",
+    algoliaOptions: { facetFilters: '["project:railgun"]' },
+  },
+  siteMetadata: {
+    title: "Railgun Documentation",
+    description:
+      "Improve loading times by caching dynamically generated or personalized web pages, dramatically reducing bandwidth usage and improving download times.",
+    author: "@cloudflare",
+    url: "https://developers.cloudflare.com/railgun",
+    image: "https://www.cloudflare.com/img/cf-twitter-card.png",
+  },
+};
--- products/terraform/docs-config.js
+++ products/terraform/docs-config.js
@@ -1,41 +1,51 @@
+const fs = require("fs");
+const productIconKey = "terraform";
+const path = require("path");
+
 module.exports = {
   product: "Terraform",
   pathPrefix: "/terraform",
-  productIconKey: "terraform",
+  productIconKey,
   contentRepo: "cloudflare/cloudflare-docs",
   contentRepoFolder: "products/terraform",
+  logoSVGContent: fs.readFileSync(
+    path.join(__dirname, `src/content/icons/${productIconKey}.svg`),
+    "utf8"
+  ),
   externalLinks: [
     {
       title: "GitHub @cloudflare/terraform-provider-cloudflare",
-      url: "https://github.com/cloudflare/terraform-provider-cloudflare"
+      url: "https://github.com/cloudflare/terraform-provider-cloudflare",
     },
     {
       title: "Cloudflare in the Terraform registry",
-      url: "https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record"
+      url:
+        "https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record",
     },
     {
       title: "Hashicorp Cloudflare integration",
-      url: "https://www.hashicorp.com/integrations/cloudflare/terraform"
+      url: "https://www.hashicorp.com/integrations/cloudflare/terraform",
     },
     {
       title: "Terraform homepage",
-      url: "https://www.terraform.io/"
+      url: "https://www.terraform.io/",
     },
     {
       title: "Cloudflare homepage",
-      url: "https://cloudflare.com"
-    }
+      url: "https://cloudflare.com",
+    },
   ],
   search: {
     indexName: "developers-cloudflare",
     apiKey: "b23088ab4d346409f9d3ece6606344c3",
-    algoliaOptions: { 'facetFilters': '["project:terraform"]'}
+    algoliaOptions: { facetFilters: '["project:terraform"]' },
   },
   siteMetadata: {
     title: "Cloudflare Terraform docs",
-    description: "With Cloudflare’s Terraform provider, you can manage your edge using the same familiar tools you use to automate the rest of your infrastructure. Define and store configuration in source code repositories like GitHub, track and version changes over time, and roll back when needed—all without needing to learn the Cloudflare APIs",
+    description:
+      "With Cloudflare’s Terraform provider, you can manage your edge using the same familiar tools you use to automate the rest of your infrastructure. Define and store configuration in source code repositories like GitHub, track and version changes over time, and roll back when needed—all without needing to learn the Cloudflare APIs",
     author: "@cloudflare",
     url: "https://developers.cloudflare.com/terraform",
-    image: "https://www.cloudflare.com/img/cf-twitter-card.png"
-  }
-}
+    image: "https://www.cloudflare.com/img/cf-twitter-card.png",
+  },
+};
