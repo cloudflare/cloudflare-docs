9a1c3a4af9725624d64890e34d6ae5dfa5d52f71
Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
2021-11-12 10:40:44 +0000

    Add search to DDoS Protection tile (#2685)

--- products/ddos-protection/docs-config.js
+++ products/ddos-protection/docs-config.js
@@ -1,29 +1,30 @@
 module.exports = {
   product: "DDoS Protection",
   pathPrefix: "/ddos-protection",
-  productLogoPathD: require('./src/content/icons/ddos-protection').pathD,
+  productLogoPathD: require("./src/content/icons/ddos-protection").pathD,
   contentRepo: "cloudflare/cloudflare-docs",
   contentRepoFolder: "products/ddos-protection",
   externalLinks: [
     {
       title: "Cloudflare DDoS Protection homepage",
-      url: "https://www.cloudflare.com/ddos/"
+      url: "https://www.cloudflare.com/ddos/",
     },
     {
       title: "Cloudflare homepage",
-      url: "https://cloudflare.com"
-    }
+      url: "https://cloudflare.com",
+    },
   ],
   search: {
-    indexName: "",
-    apiKey: "",
-    algoliaOptions: { 'facetFilters': ''}
+    indexName: "developers-cloudflare",
+    apiKey: "b23088ab4d346409f9d3ece6606344c3",
+    algoliaOptions: { facetFilters: '["project:ddos-protection"]' },
   },
   siteMetadata: {
     title: "Cloudflare DDoS Protection docs",
-    description: "Cloudflare DDoS protection secures websites, applications, and entire networks while ensuring the performance of legitimate traffic is not compromised.",
+    description:
+      "Cloudflare DDoS protection secures websites, applications, and entire networks while ensuring the performance of legitimate traffic is not compromised.",
     author: "@cloudflare",
     url: "https://developers.cloudflare.com/ddos-protection",
-    image: "https://www.cloudflare.com/img/cf-twitter-card.png"
-  }
-}
+    image: "https://www.cloudflare.com/img/cf-twitter-card.png",
+  },
+};
