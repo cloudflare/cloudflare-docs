dd85f91d11a12a93d64ab6dc4a9ad72b6b90862d
marciocloudflare <83226960+marciocloudflare@users.noreply.github.com>
2021-11-22 09:52:21 +0000

    Add search email routing (#2763)
    * Add search email routing
    
    * Update products/email-routing/docs-config.js
    
    Co-authored-by: Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
    
    Co-authored-by: Pedro Sousa <680496+pedrosousa@users.noreply.github.com>

--- products/email-routing/docs-config.js
+++ products/email-routing/docs-config.js
@@ -11,9 +11,9 @@ module.exports = {
     }
   ],
   search: {
-    indexName: "",
-    apiKey: "",
-    algoliaOptions: { facetFilters: '' },
+    indexName: "developers-cloudflare",
+    apiKey: "b23088ab4d346409f9d3ece6606344c3",
+    algoliaOptions: { 'facetFilters': '["project:email-routing"]' }
   },
   siteMetadata: {
     title: "Cloudflare Email Routing docs",
