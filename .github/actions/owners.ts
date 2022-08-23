a18e94c89ed02e72add073816cdb5380f5a59c90
Sohei Okamoto <sohei.okamoto@gmail.com>
2022-08-23 14:23:15 +0000

    [Docs] Update assign-pr owners (#5392)
    This updates `assign-pr` owners, to have product owners to be reviewers.
    
    Actions are rebuilt per internal instructions. Also, `.github/CODEOWNERS.backup` is updated to keep it in sync for `Analytics` and `Logs` section.
    
    Co-authored-by: Sohei Okamoto

--- .github/actions/owners.ts
+++ .github/actions/owners.ts
@@ -9,7 +9,7 @@
  */
 export const OWNERS: Record<string, string[]> = {
   "1.1.1.1": ["marciocloudflare"],
-  analytics: ["angelampcosta"],
+  analytics: ["angelampcosta", "soheiokamoto", "jherre", "tlozoot", "bharatnc", "victor-perov"],
   api: ["ranbel"],
   "api-shield": ["patriciasantaana"],
   "argo-smart-routing": ["maxvp"],
@@ -29,7 +29,7 @@ export const OWNERS: Record<string, string[]> = {
   fundamentals: ["kodster28"],
   images: ["marciocloudflare"],
   "load-balancing": ["patriciasantaana"],
-  logs: ["angelampcosta"],
+  logs: ["angelampcosta", "soheiokamoto", "jherre", "tlozoot", "bharatnc", "victor-perov"],
   "magic-firewall": ["dcpena"],
   "magic-transit": ["dcpena"],
   "magic-wan": ["dcpena"],
