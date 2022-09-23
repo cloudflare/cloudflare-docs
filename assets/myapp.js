47c2ffc9dc4e2c666486aa9bd6eefb6cccaf909e
Kody Jackson <kody@cloudflare.com>
2022-09-23 08:52:58 -0600

    Partial progress

--- assets/myapp.js
+++ assets/myapp.js
@@ -8,9 +8,34 @@ for (const item in paths) {
     }
 }
 
+console.log(current_path)
+
 Vue.createApp({
+    template: `<div class='header'>
+    <h1>[[ title ]]</h1>
+    <p><em>Learning path</em></p>
+    <p>[[ description ]]</p>
+    </div>
+    <div class="background">
+    <div v-for="element in elements" class="learningPathModule">
+        <div class="moduleHeader">
+            <h2 :id="element.title.toLowerCase().replaceAll(' ', '-')"><span class="DocsMarkdown--header-anchor-positioner">
+                <a
+                class="DocsMarkdown--header-anchor Link Link-without-underline"
+                :href="'#' + element.title.toLowerCase().replaceAll(' ', '-')"
+                >&#8203;​</a
+                >
+            </span>
+            <span>[[ element.title ]]</span>
+            </h2>
+        </div>
+    </div>
+    </div>`,
+    props: ['title','name'],
     data() {return {
-        this: current_path
+        title: current_path.title,
+        description: current_path.description,
+        elements: current_path.elements
     }},
     delimiters: ['[[', ']]'],
   }).mount('#vapp');
