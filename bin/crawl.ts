f58feab8234c7e3d1a6dc96447a76eb9a6533352
Greg Brimble <gbrimble@cloudflare.com>
2022-10-05 10:35:10 +0100

    Add exception to link crawler for Compatibility Dates page

--- bin/crawl.ts
+++ bin/crawl.ts
@@ -120,7 +120,11 @@ async function task(file: string) {
 
       if (!/https?/.test(resolved.protocol)) return;
 
-      if (resolved.hostname === "developers.cloudflare.com") {
+      if (
+        resolved.hostname === "developers.cloudflare.com" &&
+        // Add an exception allowing absolute URLs in the Workers Compatibility Dates page
+        url.pathname !== "/workers/platform/compatibility-dates/"
+      ) {
         messages.push({
           type: "warn",
           html: content,
