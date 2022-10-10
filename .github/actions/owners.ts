18536cff7bf796d7bd66c09e16265e76eebc748d
Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
2022-10-10 12:49:25 +0100

    Remove duplicate product entry

--- .github/actions/owners.ts
+++ .github/actions/owners.ts
@@ -52,7 +52,6 @@ export const OWNERS: Record<string, string[]> = {
   stream: ["dcpena"],
   tenant: ["kodster28"],
   terraform: ["ranbel"],
-  turnstile: ["patriciasantaana"],
   "time-services": ["kodster28"],
   turnstile: ["patriciasantaana"],
   waf: ["pedrosousa"],
