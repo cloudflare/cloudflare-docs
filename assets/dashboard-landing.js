34efff1b499d7063a57e4482cdef001c1ff61575
Kody Jackson <kody@cloudflare.com>
2022-10-18 09:05:20 -0600

    Re-organized recommendations

--- assets/dashboard-landing.js
+++ assets/dashboard-landing.js
@@ -6,6 +6,7 @@ const data = recommendations["default"]["resources"]
 const dashPathMap = data.map((x) => x.dashPath)
 
 const DEFAULT_ITEM_ID = "Default"
+let longestMatch;
 
 Vue.createApp({
   methods: {
@@ -14,25 +15,24 @@ Vue.createApp({
         get: (searchParams, prop) => searchParams.get(prop),
       });
       if (params.dash_area !== null) {
-        if (params.dash_area in this.dashPathMap) {
-          this.visibleData = data.filter((item) => item.dashPath === params.dash_area);
-        } else {
-          this.visibleData = data.filter((item) => params.dash_area.includes(item.dashPath));
-        }         
-        if (this.visibleData.length === 0) {
-          this.visibleData = data.filter((item) => item.id === "Default");
-        }
+        const matches = this.dashPathMap.filter((item) => params.dash_area.includes(item))
+        if (matches.length !== 0) {
+          longestMatch = matches.reduce(function(a, b) {
+            return a.length > b.length ? a : b
+          })
+          this.visibleData = data.find(element => element.dashPath === longestMatch );
       } else {
-        this.visibleData = data.filter((item) => item.id === "Default");
+        this.visibleData = data.find(element => element.id === DEFAULT_ITEM_ID );
       }
     }
+  }
   },
   beforeMount() {
     this.filterByParam();
   },
   template: `
         <ul class="recommendationList">
-            <li class="recommendationLink" v-for="element in visibleData[0].recommendations"><a :href='element.url_path' target='_blank'>[[ element.title ]]</a></li>
+            <li class="recommendationLink" v-for="element in visibleData.recommendations"><a :href='element.url_path' target='_blank'>[[ element.title ]]</a></li>
         </ul>`,
   data() {
     return {
