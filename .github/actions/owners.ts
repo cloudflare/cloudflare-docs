1b72ca7a062e3b844bbe2b14efa1f2c00f76ca68
jedecf <58831195+jedecf@users.noreply.github.com>
2023-02-02 11:58:13 -0800

    [SUPPORT] Adding new Support tile (#7497)
    * [SUPPORT] Adding new Support tile
    
    * Update content/support/_index.md
    
    Co-authored-by: Kody Jackson <kody.s.jackson@gmail.com>
    
    * Update data/support.yml
    
    Co-authored-by: Kody Jackson <kody.s.jackson@gmail.com>
    
    * Update data/support.yml
    
    Co-authored-by: Kody Jackson <kody.s.jackson@gmail.com>
    
    * Update data/support.yml
    
    Co-authored-by: Kody Jackson <kody.s.jackson@gmail.com>
    
    * [SUPPORT] Adding new tile
    
    ---------
    
    Co-authored-by: Jamie Ede <jedecf>
    Co-authored-by: Kody Jackson <kody.s.jackson@gmail.com>

--- .github/actions/owners.ts
+++ .github/actions/owners.ts
@@ -55,6 +55,7 @@ export const OWNERS: Record<string, string[]> = {
   spectrum: ["angelampcosta"],
   ssl: ["kodster28"],
   stream: ["dcpena"],
+  support: ["jedecf"],
   tenant: ["kodster28"],
   terraform: ["ranbel"],
   "time-services": ["kodster28"],
