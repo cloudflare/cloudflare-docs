643cb8f1da18872e7920390ee4f441beb307e28b
Kody Jackson <kody@cloudflare.com>
2023-02-08 09:33:50 -0700

    Removed from json collector

--- assets/json-collector.ts
+++ assets/json-collector.ts
@@ -2,7 +2,6 @@ import * as get_started from "./json/get-started.json";
 import * as optimize_site_speed from "./json/optimize-site-speed.json";
 import * as application_security from "./json/application-security.json";
 import * as replace_vpn from "./json/replace-vpn.json";
-import * as partners_integrations from "./json/technology-partner-integrations.json";
 import * as learn_bots from "./data/learning-paths/learn_bots.json";
 
 let learning_paths = [
@@ -10,7 +9,6 @@ let learning_paths = [
   optimize_site_speed["default"],
   application_security["default"],
   replace_vpn["default"],
-  partners_integrations["default"],
   learn_bots
 ];
 
