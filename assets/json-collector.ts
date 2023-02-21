712751526238787b7a0c6ffb43ab3e5962d8a5b2
Kody Jackson <kody@cloudflare.com>
2023-02-21 12:44:18 -0700

    updating path

--- assets/json-collector.ts
+++ assets/json-collector.ts
@@ -2,7 +2,7 @@ import * as get_started from "./json/get-started.json";
 import * as optimize_site_speed from "./json/optimize-site-speed.json";
 import * as application_security from "./json/application-security.json";
 import * as replace_vpn from "./json/replace-vpn.json";
-import * as load_balancing from "../data/learning-paths/load-balancing.json";
+import * as load_balancing from "./data/learning-paths/load-balancing.json";
 
 let learning_paths = [
   get_started,
