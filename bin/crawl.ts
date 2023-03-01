f338788b3a2d75d04b39a2f775828dda91b514a8
Kody Jackson <kody@cloudflare.com>
2023-03-01 10:40:49 -0700

    testing updates to crawl

--- bin/crawl.ts
+++ bin/crawl.ts
@@ -33,10 +33,11 @@ async function walk(dir: string) {
   await Promise.all(
     files.map(async (name) => {
       let abs = join(dir, name);
-      if (name.endsWith(".html")) return task(abs);
-
-      let stats = await fs.stat(abs);
-      if (stats.isDirectory()) return walk(abs);
+      if (!abs.includes("/support/other-languages")) {
+        if (name.endsWith(".html")) return task(abs);
+        let stats = await fs.stat(abs);
+        if (stats.isDirectory()) return walk(abs);
+      }
     })
   );
 }
