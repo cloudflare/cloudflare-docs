83854b3315677e76cecd37861970a1f04d4075f8
Kody Jackson <kody@cloudflare.com>
2023-03-08 11:23:30 -0700

    [Learning paths] Bot mgmt guide (#7809)
    * Added bot learning path + rough draft for concepts section
    
    * Added redirect link
    
    * Fixed broken links
    
    * Updated the path grid layout
    
    * Fleshed out content module
    
    * more content changes
    
    * Partial planning section
    
    * Updated site content
    
    * Finished planning section
    
    * small tweaks
    
    * Finished rough draft of other content
    
    * Small typos
    
    * Added info from PCX-6058
    
    * Added static resource protection to firewall fields reference
    
    * Update content/ruleset-engine/rules-language/fields.md
    
    Co-authored-by: Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
    
    * Update content/bots/_partials/_bot-log-fields.md
    
    Co-authored-by: Patricia Santa Ana <103445940+patriciasantaana@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Patricia Santa Ana <103445940+patriciasantaana@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
    Co-authored-by: Patricia Santa Ana <103445940+patriciasantaana@users.noreply.github.com>

--- assets/json-collector.ts
+++ assets/json-collector.ts
@@ -2,14 +2,18 @@ import * as get_started from "data/learning-paths/get-started.json";
 import * as optimize_site_speed from "data/learning-paths/optimize-site-speed.json";
 import * as application_security from "data/learning-paths/application-security.json";
 import * as replace_vpn from "data/learning-paths/replace-vpn.json";
+import * as bot_management from "data/learning-paths/bot-management.json";
 import * as load_balancing from "data/learning-paths/load-balancing.json";
+import * as workers from "data/learning-paths/workers.json";
 
 let learning_paths = [
   get_started,
+  workers,
+  bot_management,
+  load_balancing,
+  replace_vpn,
   optimize_site_speed,
   application_security,
-  replace_vpn,
-  load_balancing,
 ];
 
 export { learning_paths };
---
+++ content/learning-paths/modules/security/bot-management-setup/_index.md
@@ -0,0 +1,10 @@
+---
+title: Setup
+pcx_content_type: overview
+weight: 3
+layout: learning-module
+---
+
+# Setup
+
+{{<learning-module-summary>}}
