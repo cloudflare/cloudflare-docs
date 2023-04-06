9fc92563f79a438d7e1e1f03314816d85ed96f3a
Kristian Freeman <kristian@kristianfreeman.com>
2023-04-06 10:10:46 -0500

    Fix results

--- assets/search.ts
+++ assets/search.ts
@@ -16,73 +16,7 @@
       appId: '8MU1G3QO9P',
       apiKey: '045e8dbec8c137a52f0f56e196d7abe0',
       container: '#algolia',
-      debug: true,
-
-      // algoliaOptions: {
-      //   facetFilters: filters || '',
-      // },
-
-      // autocompleteOptions: {
-      //   // https://github.com/algolia/autocomplete.js#global-options
-      //   autoselect: true,
-      //   openOnFocus: true,
-      //   clearOnSelected: false,
-      //   tabAutocomplete: false,
-
-      //   appendTo: '.' + element.parentNode.className,
-      //   hint: false,
-
-      //   autoselectOnBlur: matchMedia('(pointer: course)').matches,
-      // },
-
-      // // https://docsearch.algolia.com/docs/behavior
-      // handleSelected(input, event, suggestion, datasetNumber, context) {
-      //   let ctx = new URL(suggestion.url);
-
-      //   algolia.input.autocomplete.setVal('');
-      //   algolia.input[0].blur();
-
-      //   // no scroll if is H1 tag
-      //   if (suggestion.isLvl0) ctx.hash = '';
-
-      //   // redirect to new path
-      //   return location.assign(ctx.pathname + ctx.search + ctx.hash);
-      // },
-
-      transformItems(items) {
-        // Remove empty results
-        for (let len = items.length; len-- > 0;) {
-          let info = items[len].hierarchy;
-          if (!info.lvl0 && !info.lvl1) {
-            items.splice(len, 1);
-          }
-        }
-      },
     });
-
-    // let input = algolia.input[0];
-    // let wrapper = algolia.autocomplete.autocomplete.getWrapper();
-
-    // algolia.autocomplete.on('autocomplete:shown', () => {
-    //   wrapper.setAttribute('data-expanded', true);
-    // });
-
-    // algolia.autocomplete.on('autocomplete:closed', () => {
-    //   wrapper.setAttribute('data-expanded', false);
-    // });
-
-    // addEventListener('keydown', ev => {
-    //   if (ev.target === input) return;
-
-    //   let key = ev.which;
-
-    //   // is '/' or SHIFT+'s'
-    //   if (key === 191 || (ev.shiftKey && key === 83)) {
-    //     ev.preventDefault();
-    //     window.scrollTo(0, 0);
-    //     input.focus();
-    //   }
-    // });
 
     // instantiate mobile search button
     let button = $('#MobileSearch')
