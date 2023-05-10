a2631e66ee34c3af0ab92b4b8482b11f3f15c43c
Kody Jackson <kody@cloudflare.com>
2023-05-10 14:50:13 -0600

    Updated styles

--- assets/presentation-mode.ts
+++ assets/presentation-mode.ts
@@ -6,47 +6,63 @@ const presentationMode = params.get("presentation_mode");
 const currentLearningPath = params.get("learning_path");
 let currentPathData;
 
-  if (currentLearningPath !== null) {
-    for (const path in paths) {
-      if (paths[path]["uid"] === currentLearningPath) {
-        currentPathData = paths[path];
-      }
+if (currentLearningPath !== null) {
+  for (const path in paths) {
+    if (paths[path]["uid"] === currentLearningPath) {
+      currentPathData = paths[path];
     }
   }
-
-function hasChildInArray(element, array) {
-  return Array.from(element.querySelectorAll('*')).some((child) => array.includes(child));
 }
 
-function cuePresentationMode () {
-  
-  if (presentationMode) {
+function hasChildInArray(element, array) {
+  return Array.from(element.querySelectorAll("*")).some((child) =>
+    array.includes(child)
+  );
+}
 
-    const navigationButtons = document.getElementsByClassName("learningNavigation Button")
+function cuePresentationMode() {
+  if (presentationMode) {
+    const navigationButtons = document.getElementsByClassName(
+      "learningNavigation Button"
+    );
+    
     if (navigationButtons) {
       for (const item of navigationButtons) {
-        const currentHref = item.getAttribute("href").split('?').shift();
+        const currentHref = item.getAttribute("href").split("?").shift();
         item.setAttribute(
           "href",
-          currentHref + "?learning_path=" + currentLearningPath + "&presentation_mode=true"
+          currentHref +
+            "?learning_path=" +
+            currentLearningPath +
+            "&presentation_mode=true"
         );
       }
     }
 
-
     // Get the elements to keep
-const elementsToKeep = Array.from(document.querySelectorAll('h1, .progressIndicator *, #presentationMode *'));
+    const elementsToKeep = Array.from(
+      document.querySelectorAll("h1, .progressIndicator *, #presentationMode *")
+    );
 
-// Loop through all elements in the body and remove those that are not in the 'elementsToKeep' list
-document.querySelectorAll('body *').forEach((element) => {
-  if (!elementsToKeep.includes(element) && !hasChildInArray(element, elementsToKeep)) {
-    element.remove();
-  }
-  });
-  const page = document.getElementById('DocsPage');
-  page.style.visibility = "visible"
-  }
+    // Loop through all elements in the body and remove those that are not in the 'elementsToKeep' list
+    document.querySelectorAll("body *").forEach((element) => {
+      if (
+        !elementsToKeep.includes(element) &&
+        !hasChildInArray(element, elementsToKeep)
+      ) {
+        element.remove();
+      }
+    });
 
-};
+    const docBody = document.getElementById("DocsBody")
+    
+
+    docBody.classList.add("PresentationMode")
+
+
+    const page = document.getElementById("DocsPage");
+    page.style.visibility = "visible";
+  }
+}
 
 setTimeout(cuePresentationMode, 500);
