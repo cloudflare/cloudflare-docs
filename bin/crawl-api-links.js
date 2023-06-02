f8f0d6c127fa79d63288e6d4c3a2b1d46a6b801c
Kody Jackson <kody@cloudflare.com>
2023-06-02 16:30:35 -0600

    Speed up link checking by not re-evaluating api links if already evaluated

--- bin/crawl-api-links.js
+++ bin/crawl-api-links.js
@@ -1,34 +1,45 @@
 import puppeteer from 'puppeteer';
 
+const navigationTimeout = 120000; // Set the navigation timeout to 60 seconds (60000 milliseconds)
+
 async function checkLinks() {
   const browser = await puppeteer.launch({
     headless: 'new',
   });
   const page = await browser.newPage();
-  const navigationTimeout = 1200000;
 
-  const sitemapUrl = 'https://developers.cloudflare.com/sitemap.xml'; // Replace with your sitemap URL
-  await page.goto(sitemapUrl);
+  const sitemapUrl = 'http://developers.cloudflare/sitemap.xml';
+  await page.goto(sitemapUrl, { timeout: navigationTimeout });
 
   const sitemapLinks = await page.$$eval('url loc', (elements) =>
     elements.map((el) => el.textContent)
   );
 
+  const visitedLinks = new Set(); // Set to store visited links
   const brokenLinks = [];
 
   for (const link of sitemapLinks) {
-    if (link) {
+    if (!link) {
+      continue; // Skip if the link is empty
+    }
+
     console.log(`Processing link: ${link}`);
-    await page.goto(link, { waitUntil: 'networkidle0' });
+    await page.goto(link, { waitUntil: 'networkidle0', timeout: navigationTimeout });
+    visitedLinks.add(link); // Add the link to the visited set
 
     const pageLinks = await page.$$eval('a', (elements) =>
       elements.map((el) => el.href)
     );
 
     for (const pageLink of pageLinks) {
-      if (pageLink) {
-      if (pageLink.includes("https://developers.cloudflare.com/api/operations")) {
-      await page.goto(pageLink, { waitUntil: 'networkidle0', navigationTimeout });
+      if (!pageLink || visitedLinks.has(pageLink)) {
+        continue; // Skip if the pageLink is empty or has already been visited
+      } else if (!pageLink.includes("https://developers.cloudflare.com/api/operations")) {
+        continue; // Skip if the pageLink isn't from the api docs
+      }
+
+      await page.goto(pageLink, { waitUntil: 'networkidle0', timeout: navigationTimeout });
+      visitedLinks.add(pageLink); // Add the pageLink to the visited set
 
       const statusCode = await page.evaluate(() => {
         return {
@@ -42,9 +53,6 @@ async function checkLinks() {
       }
     }
   }
-}
-}
-}
 
   await browser.close();
 
@@ -60,4 +68,4 @@ async function checkLinks() {
 checkLinks().catch((error) => {
   console.error(error);
   process.exit(1);
-});
+});  
