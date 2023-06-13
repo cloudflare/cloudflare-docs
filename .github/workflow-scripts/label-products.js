fe7d309b8b345ef3069fcb76ab9345aee533b3ef
Kody Jackson <kody@cloudflare.com>
2023-06-13 15:24:22 -0600

    [GitHub] Auto-size PR (#9339)
    * Test commit
    
    * update script
    
    * Updated for no previous label
    
    * Make optional for merging PR
    
    * fix error
    
    * try bumping to v6
    
    * another test
    
    * use another action
    
    * bug?
    
    * doesn't apply to drafts?
    
    * need existing labels?
    
    * Update
    
    * part of another workflow?
    
    * bleh
    
    * test another implementation
    
    * update script
    
    * another test
    
    * test js implementation
    
    * update
    
    * update location
    
    * rework script
    
    * another test
    
    * moved script location
    
    * Updates
    
    * update imports
    
    * update imports
    
    * Update dependencies
    
    * small update
    
    * Update script for token
    
    * reference for token
    
    * maybe it's working direectory
    
    * pass as param
    
    * move all to params
    
    * one more change
    
    * Update referencing token
    
    * Another update
    
    * check for changes
    
    * test larger PR
    
    * test even larger PR
    
    * Update to remove all previous labels
    
    * restore content
    
    * small cleanup
    
    * test small change
    
    * small grammar
    
    * one more small update
    
    * update page
    
    * update again
    
    * Test labelling for products
    
    * updated changed subfolders
    
    * another tweak
    
    * try refactoring
    
    * more tinkering
    
    * my error
    
    * use pre-existing structure
    
    * update reference
    
    * another approach
    
    * pull files earlier
    
    * Another update
    
    * Added a cache update
    
    * Remove cache
    
    * What about a new label?
    
    * let's break the check
    
    * restore checks

---
+++ .github/workflow-scripts/label-products.js
@@ -0,0 +1,87 @@
+import * as github from '@actions/github';
+
+function getTopLevelFolder(path) {
+  const parts = path.split('/');
+  return parts[0];
+}
+
+function getSubFolder(path) {
+  const parts = path.split('/');
+  return parts[1];
+}
+
+function getChangedSubFolders(files) {
+  const changedFolders = new Set();
+
+  for (const file of files) {
+    const path = file.filename;
+    const topLevelFolder = getTopLevelFolder(path);
+
+    // Check if the file is within the top-level /content folder
+    if (topLevelFolder === 'content') {
+      const subFolder = getSubFolder(path);
+      changedFolders.add(subFolder);
+    }
+  }
+
+  return Array.from(changedFolders);
+}
+
+async function run() {
+  try {
+    const ctx = github.context;
+    const token = process.env.GITHUB_TOKEN;
+    const octokit = github.getOctokit(token);
+    const pr = ctx.payload.pull_request;
+    const prNumber = pr.number;
+    const files = await octokit.paginate(octokit.rest.pulls.listFiles, {
+        ...ctx.repo,
+        pull_number: pr.number,
+        per_page: 100
+      });
+
+    // Get the changed sub-folders within the top-level /content folder
+    const changedFolders = getChangedSubFolders(files);
+
+    // ...
+
+    // Label the PR based on the changed sub-folders
+    await labelPRSubFolders(octokit, ctx.repo, prNumber, changedFolders);
+
+    // ...
+  } catch (error) {
+    console.error('An error occurred:', error);
+    process.exit(1);
+  }
+}
+
+async function labelPRSubFolders(octokit, repo, prNumber, changedFolders) {
+  const labelPrefix = 'product:';
+  const labelsToRemove = [];
+
+  for (const label of github.context.payload.pull_request.labels) {
+    if (label.name.startsWith(labelPrefix)) {
+      labelsToRemove.push(label.name);
+    }
+  }
+
+  for (const labelToRemove of labelsToRemove) {
+    await octokit.rest.issues.removeLabel({
+      ...repo,
+      issue_number: prNumber,
+      name: labelToRemove
+    });
+  }
+
+  for (const folder of changedFolders) {
+    const label = labelPrefix + folder;
+
+    await octokit.rest.issues.addLabels({
+      ...repo,
+      issue_number: prNumber,
+      labels: [label]
+    });
+  }
+}
+
+run();
