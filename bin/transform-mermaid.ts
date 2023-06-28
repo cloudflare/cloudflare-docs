53348b6c764f0d0fe6a1d6d17d33e6963b1eceff
Kody Jackson <kody@cloudflare.com>
2023-06-28 13:07:14 -0600

    another test

--- bin/transform-mermaid.ts
+++ bin/transform-mermaid.ts
@@ -1,20 +1,51 @@
 import * as fs from 'fs';
-import * as path from 'path';
+import { join, resolve, parse, relative } from 'path';
 import { execSync } from 'child_process';
 
 // Set the source and destination directories
-const sourceDir = '/content';
-const destinationDir = '/content';
+const root = resolve('.');
+const contentDirectory = join(root, 'content/');
+const sourceDir = contentDirectory;
+const destinationDir = contentDirectory;
 
-// Get a list of markdown files in the source directory
-const markdownFiles = fs.readdirSync(sourceDir).filter((filename) => filename.endsWith('.md'));
+// Process Markdown files recursively
+const processMarkdownFiles = (directory) => {
+  const files = fs.readdirSync(directory);
 
-// Transform each markdown file using mermaid-cli
-markdownFiles.forEach((filename) => {
-  const sourceFile = path.join(sourceDir, filename);
-  const destinationFile = path.join(destinationDir, path.parse(filename).name + '.html');
-  const command = `mmdc -i "${sourceFile}" -o "${destinationFile}"`;
+  files.forEach((filename) => {
+    const filePath = join(directory, filename);
+    const stats = fs.statSync(filePath);
 
-  // Execute the command synchronously
-  execSync(command);
-});
+    if (stats.isDirectory()) {
+      processMarkdownFiles(filePath); // Recursively process subdirectories
+    } else if (stats.isFile() && filename.endsWith('.md')) {
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+
+      // Check if the file contains a Mermaid snippet
+      if (fileContent.includes('```mermaid')) {
+        const sourceFile = filePath;
+        const relativePath = relative(sourceDir, sourceFile);
+        const destinationFile = join(destinationDir, relativePath);
+        const command = `mmdc -i "${sourceFile}" -o "${destinationFile}"`;
+
+        // Execute the command synchronously
+        execSync(command);
+
+        // Read the transformed content
+        const transformedContent = fs.readFileSync(destinationFile, 'utf8');
+
+        // Update SVG reference in the transformed Markdown file
+        const updatedContent = transformedContent.replace(
+          /!\[([^\]]+)\]\(.\//g,
+          (_, altText) => `![${altText}](../`
+        );
+
+        // Write the updated content back to the file
+        fs.writeFileSync(destinationFile, updatedContent);
+      }
+    }
+  });
+};
+
+// Start processing from the root directory
+processMarkdownFiles(sourceDir);
