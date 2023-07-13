fa52362a930fb2b1f7d43c79814632ff375d68db
Kody Jackson <kody@cloudflare.com>
2023-07-13 09:59:10 -0600

    Update assignment script

--- .github/actions/assign-pr/index.ts
+++ .github/actions/assign-pr/index.ts
@@ -1,9 +1,9 @@
 // NOTE: This is the source file!
 // ~> Run `npm run build` to produce `index.js`
 
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import { OWNERS, REVIEWERS } from '../owners';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { OWNERS, REVIEWERS } from "../owners";
 
 type Octokit = ReturnType<typeof github.getOctokit>;
 
@@ -16,22 +16,27 @@ type Options = {
 };
 
 // @see https://octokit.github.io/rest.js/v18#pulls-list-files
-async function list(client: Octokit, options: Options, products?: Set<string>): Promise<Set<string>> {
-  products = products || new Set;
+async function list(
+  client: Octokit,
+  options: Options,
+  products?: Set<string>
+): Promise<Set<string>> {
+  products = products || new Set();
   options.page = options.page || 1;
 
   let limit = (options.per_page = options.per_page || 100);
   let res = await client.rest.pulls.listFiles(options);
 
   // retrieve the product name
-  let i=0, len=res.data.length;
-  for (let file, tmp: string|void; i < len; i++) {
+  let i = 0,
+    len = res.data.length;
+  for (let file, tmp: string | void; i < len; i++) {
     file = res.data[i];
 
     tmp = parse(file.filename);
     if (tmp) products.add(tmp);
 
-    if (tmp = file.previous_filename) {
+    if ((tmp = file.previous_filename)) {
       tmp = parse(tmp);
       if (tmp) products.add(tmp);
     }
@@ -49,7 +54,9 @@ async function list(client: Octokit, options: Options, products?: Set<string>): 
  * @note code runs in posix environment ("/" not "\\")
  */
 function parse(filename: string): string | void {
-  return (/^data[/](.*).ya?ml$/.exec(filename) || /^content[/]([^\/]+)[/]/.exec(filename) || [])[1];
+  return (/^data[/](.*).ya?ml$/.exec(filename) ||
+    /^content[/]([^\/]+)[/]/.exec(filename) ||
+    [])[1];
 }
 
 // https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads#webhook-payload-object-34
@@ -57,10 +64,10 @@ function parse(filename: string): string | void {
 
 (async function () {
   try {
-    const token = core.getInput('GITHUB_TOKEN', { required: true });
+    const token = core.getInput("GITHUB_TOKEN", { required: true });
 
     const payload = github.context.payload;
-    console.log('event payload:', JSON.stringify(payload, null, 2));
+    console.log("event payload:", JSON.stringify(payload, null, 2));
 
     const { repository, pull_request } = payload;
     if (!pull_request) throw new Error('Missing "pull_request" object!');
@@ -73,7 +80,7 @@ function parse(filename: string): string | void {
     const PCX = new Set<string>();
 
     for (let p in OWNERS) {
-      OWNERS[p].forEach(x => PCX.add(x));
+      OWNERS[p].forEach((x) => PCX.add(x));
     }
 
     const reviewers = new Set<string>();
@@ -99,25 +106,31 @@ function parse(filename: string): string | void {
       }
 
       if (owners_list.length > 0) {
-        owners_list.forEach(x => reviewers.add(x));
+        owners_list.forEach((x) => reviewers.add(x));
       }
 
       let reviewers_list = REVIEWERS[slug];
 
       if (reviewers_list && reviewers_list.length > 0) {
-        reviewers_list.forEach(x => reviewers.add(x));
+        reviewers_list.forEach((x) => reviewers.add(x));
       }
     }
 
     if (PCX.has(author)) {
-      const pcx_and_reviewers = new Set([...PCX, ...reviewers]);
-      pcx_and_reviewers.delete(author); // no self-review
-      console.log('~> request PCX team review');
-      return await client.rest.issues.addLabels({
+      console.log("~> request PCX team review");
+      await client.rest.issues.addLabels({
         ...github.context.repo,
         issue_number: prnumber,
-        labels: ["pcx_team_review"]
+        labels: ["pcx_team_review"],
       });
+      if (reviewers.size > 0) {
+        await client.rest.pulls.requestReviewers({
+          repo: repository.name,
+          owner: repository.owner.login,
+          pull_number: prnumber,
+          reviewers: [...reviewers],
+        });
+      }
     }
 
     // ---
@@ -139,20 +152,20 @@ function parse(filename: string): string | void {
 
     if (reviewers.size === 0) {
       if (requested.size > 0) {
-        console.log('~> had reviewers at creation');
+        console.log("~> had reviewers at creation");
       } else if (products.size > 0) {
         console.log('~> ping "haleycode" for assignment');
         await client.rest.issues.addAssignees({
           repo: repository.name,
           owner: repository.owner.login,
           issue_number: prnumber,
-          assignees: ['haleycode'],
+          assignees: ["haleycode"],
         });
       } else {
-        console.log('~> no products changed; engineering?');
+        console.log("~> no products changed; engineering?");
       }
     } else {
-      console.log('~> request individual reviews');
+      console.log("~> request individual reviews");
       await client.rest.pulls.requestReviewers({
         repo: repository.name,
         owner: repository.owner.login,
@@ -167,7 +180,7 @@ function parse(filename: string): string | void {
       });
     }
 
-    console.log('DONE~!');
+    console.log("DONE~!");
   } catch (error) {
     core.setFailed(error.message);
   }
