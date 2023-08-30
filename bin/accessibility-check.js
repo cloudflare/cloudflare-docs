2e898edbee1b46526b5c3880fc7b0ed0c7dcd51d
Kody Jackson <kody@cloudflare.com>
2023-08-30 16:37:30 -0600

    test fix for script

--- bin/accessibility-check.js
+++ bin/accessibility-check.js
@@ -1,21 +1,24 @@
-import pa11y from "pa11y"
-import core from "@actions/core";
-import axios from "axios"
+import pa11y from "pa11y";
+import axios from "axios";
 import { parseString } from 'xml2js';
 import { EventEmitter } from 'events';
 
 const myEmitter = new EventEmitter();
-myEmitter.setMaxListeners(20); // Increase
+myEmitter.setMaxListeners(20);
 
 const sitemapUrl = 'https://developers.cloudflare.com/sitemap.xml';
 
-async function processUrl(url) {
-    try {
-        const results = await pa11y(url);
-        console.log(results)
-    } catch (error) {
-        console.log("error")
-    }
+async function processUrlBatch(urls) {
+    const promises = urls.map(async (url) => {
+        try {
+            const results = await pa11y(url);
+            console.log(results);
+        } catch (error) {
+            console.log("error");
+        }
+    });
+
+    await Promise.all(promises);
 }
 
 axios.get(sitemapUrl)
@@ -28,7 +31,7 @@ axios.get(sitemapUrl)
                 }
 
                 const urls = result.urlset.url.map(urlObj => urlObj.loc[0]);
-                urls.forEach(processUrl);
+                processUrlBatch(urls);
             });
         } else {
             console.error('Failed to fetch sitemap. Status code:', response.status);
