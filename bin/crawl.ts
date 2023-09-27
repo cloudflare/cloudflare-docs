b8223dcc1bb85926c0998ae0f24afcac25757fd7
Kody Jackson <kody@cloudflare.com>
2023-09-27 09:01:21 -0600

    Constellation fix (#10914)
    * Constellation fix
    
    * Add some meta properties
    
    * Updated product group name

--- bin/crawl.ts
+++ bin/crawl.ts
@@ -32,7 +32,6 @@ async function walk(dir: string) {
   let files = await fs.readdir(dir);
   await Promise.all(
     files.map(async (name) => {
-      if (name.includes("constellation")) { return; }
       let abs = join(dir, name);
       let supportOtherLangsPath = "/support/other-languages";
       if (process.platform === "win32") {
