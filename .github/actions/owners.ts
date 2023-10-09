d14affdb6a94d3fc0bc8a26cce1f1672a430f21a
Kody Jackson <kody@cloudflare.com>
2023-10-09 10:07:21 -0600

    Update version mgmt ownership

--- .github/actions/owners.ts
+++ .github/actions/owners.ts
@@ -73,7 +73,7 @@ export const OWNERS: Record<string, string[]> = {
   terraform: ["ranbel"],
   "time-services": ["kodster28"],
   turnstile: ["patriciasantaana"],
-  "version-management": ["kodster28"],
+  "version-management": ["patriciasantaana"],
   waf: ["pedrosousa"],
   "waiting-room": ["angelampcosta"],
   "warp-client": ["haleycode"],
