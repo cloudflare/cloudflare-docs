92d10430522a3bed87a3d280a44b69b88911b0d8
Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
2023-11-23 16:47:20 +0000

    [Chore] Update redirect validations

--- bin/find-infinite-redirects.ts
+++ bin/find-infinite-redirects.ts
@@ -3,21 +3,39 @@ import { readFile } from 'fs/promises';
 async function main( ){
   const redirects = await readFile('content/_redirects', { encoding: 'utf-8' });
 
-  let found = 0;
+  let numInfiniteRedirects = 0;
+  let numUrlsWithFragment = 0;
   for (const line of redirects.split('\n')) {
     if (line.startsWith('#') || line.trim() === '') continue;
 
     const [from, to] = line.split(' ');
 
     if (from === to) {
-      console.log(`Found infinite redirect: ${from} -> ${to}`);
-      found++;
+      console.log(`✘ Found infinite redirect:\n    ${from} -> ${to}`);
+      numInfiniteRedirects++;
+    }
+
+    if (from.includes('#')) {
+      console.log(`✘ Found source URL with fragment:\n    ${from}`);
+      numUrlsWithFragment++;
     }
   }
 
-  if (found) {
-    console.log(`\nFound ${found} infinite redirects, please fix them before merging :)`);
+  if (numInfiniteRedirects || numUrlsWithFragment) {
+    console.log("\nDetected errors:");
+
+    if (numInfiniteRedirects > 0) {
+      console.log(`- ${numInfiniteRedirects} infinite redirect(s)`);
+    }
+
+    if (numUrlsWithFragment > 0) {
+      console.log(`- ${numUrlsWithFragment} source URL(s) with a fragment`);
+    }
+
+    console.log("\nPlease fix the errors above before merging :)");
     process.exit(1);
+  } else {
+    console.log("\nDone!");
   }
 }
 
