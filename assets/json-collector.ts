40d32f444a6e9da0573766ead0590de8f95faa75
Jason Farber <jason@cloudflare.com>
2023-12-05 18:14:58 +0000

    updating file name

--- assets/json-collector.ts
+++ assets/json-collector.ts
@@ -1,7 +1,7 @@
 import * as get_started from "data/learning-paths/get-started.json";
 import * as optimize_site_speed from "data/learning-paths/optimize-site-speed.json";
 import * as application_security from "data/learning-paths/application-security.json";
-import * as replace_vpn from "data/learning-paths/replace-your-vpn.json";
+import * as replace_vpn from "data/learning-paths/replace-vpn.json";
 import * as dns_filtering from "data/learning-paths/dns-filtering.json";
 import * as load_balancing from "data/learning-paths/load-balancing.json";
 import * as workers from "data/learning-paths/workers.json";
