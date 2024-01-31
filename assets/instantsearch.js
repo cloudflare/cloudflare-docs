7f135c07df05e8b0105420728bdae84980c64787
Kody Jackson <kody@cloudflare.com>
2024-01-31 10:08:16 -0700

    updated template for api endpoints in index

--- assets/instantsearch.js
+++ assets/instantsearch.js
@@ -1,12 +1,15 @@
-import { createDropdown } from './instantsearchdropdowns';
+import { createDropdown } from "./instantsearchdropdowns";
 
-const searchClient = algoliasearch('8MU1G3QO9P', '4edb0a6cef3338ff4bcfbc6b3d2db56b');
-const indexName = 'crawler_Dev Docs -> Instantsearch.js'
+const searchClient = algoliasearch(
+  "8MU1G3QO9P",
+  "4edb0a6cef3338ff4bcfbc6b3d2db56b"
+);
+const indexName = "crawler_Dev Docs -> Instantsearch.js";
 const { searchBox } = instantsearch.widgets;
 const { hits } = instantsearch.widgets;
 const { configure } = instantsearch.widgets;
 const { pagination } = instantsearch.widgets;
-const {refinementList} = instantsearch.widgets;
+const { refinementList } = instantsearch.widgets;
 
 const search = instantsearch({
   indexName: indexName,
@@ -18,10 +21,15 @@ const search = instantsearch({
         const indexUiState = uiState[indexName];
         return {
           q: indexUiState.query,
-          product: indexUiState.refinementList && indexUiState.refinementList.product,
-          product_group: indexUiState.refinementList && indexUiState.refinementList.product_group,
-          content_type: indexUiState.refinementList && indexUiState.refinementList.content_type,
-        }
+          product:
+            indexUiState.refinementList && indexUiState.refinementList.product,
+          product_group:
+            indexUiState.refinementList &&
+            indexUiState.refinementList.product_group,
+          content_type:
+            indexUiState.refinementList &&
+            indexUiState.refinementList.content_type,
+        };
       },
       routeToState(routeState) {
         return {
@@ -30,8 +38,8 @@ const search = instantsearch({
             refinementList: {
               product: routeState.product,
               product_group: routeState.product_group,
-              content_type: routeState.content_type
-            }
+              content_type: routeState.content_type,
+            },
           },
         };
       },
@@ -47,63 +55,89 @@ const productDropdown = createDropdown(refinementList, {
 
 const productGroupDropdown = createDropdown(refinementList, {
   closeOnChange: () => window.innerWidth >= MOBILE_WIDTH,
-  buttonText: 'Product group',
+  buttonText: "Product group",
 });
 
 const contentTypeDropdown = createDropdown(refinementList, {
   closeOnChange: () => window.innerWidth >= MOBILE_WIDTH,
-  buttonText: 'Content type',
+  buttonText: "Content type",
 });
 
 search.addWidgets([
   searchBox({
-    container: "#searchbox"
+    container: "#searchbox",
   }),
-
   hits({
     container: "#hits",
     templates: {
       item(hit, { html, components }) {
-        return html`
-        <div class="searchResult">
-        <a href=${hit.url}>
-        <h3>
-            ${components.Highlight({ attribute: 'title', hit })}
-          </h3>
-          </a>
-          <p>${components.Snippet({ attribute: 'content', hit })}</p>
-          <div class="additionalSearchContent">
-          <p><strong>Product</strong>: ${components.Highlight({ attribute: 'product', hit })}</p>
-          <p><strong>Product group</strong>: ${components.Highlight({ attribute: 'product_group', hit })}</p>
-          <p><strong>Content type</strong>: ${components.Highlight({ attribute: 'content_type', hit })}</p>
-          </div>
-          <a href=${hit.url}>${hit.url}</a>
-          </div>
-        `;
+        if (hit.path.startsWith("/api/operations/")) {
+          return html`
+            <div class="searchResult">
+              <a href=${hit.url}>
+                <h3>${components.Highlight({ attribute: "title", hit })}</h3>
+              </a>
+              <p>${components.Snippet({ attribute: "content", hit })}</p>
+              <div class="additionalSearchContent">
+                <p>
+                  <strong>Operation</strong>: ${components.Highlight({ attribute: "operation", hit })}
+                </p>
+                <p>
+                  <strong>Endpoint group</strong>: ${components.Highlight({ attribute: "parent_tag", hit })}
+                </p>
+                <p>
+                  <strong>Content type</strong>: ${components.Highlight({ attribute: "content_type", hit })}
+                </p>
+              </div>
+              <a href=${hit.url}>${hit.url}</a>
+            </div>
+          `;
+        } else {
+          return html`
+            <div class="searchResult">
+              <a href=${hit.url}>
+                <h3>${components.Highlight({ attribute: "title", hit })}</h3>
+              </a>
+              <p>${components.Snippet({ attribute: "content", hit })}</p>
+              <div class="additionalSearchContent">
+                <p>
+                  <strong>Product</strong>: ${components.Highlight({ attribute: "product", hit })}
+                </p>
+                <p>
+                  <strong>Product group</strong>: ${components.Highlight({ attribute: "product_group", hit })}
+                </p>
+                <p>
+                  <strong>Content type</strong>: ${components.Highlight({ attribute: "content_type", hit })}
+                </p>
+              </div>
+              <a href=${hit.url}>${hit.url}</a>
+            </div>
+          `;
+        }
       },
     },
   }),
   configure({
-    hitsPerPage: 8,
-    attributesToSnippet: ['content:30'],
+    hitsPerPage: 10,
+    attributesToSnippet: ["content:30"],
   }),
   productDropdown({
-    container: '#product',
-    attribute: 'product',
+    container: "#product",
+    attribute: "product",
     searchable: true,
   }),
   productGroupDropdown({
-    container: '#product_group',
-    attribute: 'product_group',
+    container: "#product_group",
+    attribute: "product_group",
     searchable: true,
   }),
   contentTypeDropdown({
-    container: '#content_type',
-    attribute: 'content_type',
+    container: "#content_type",
+    attribute: "content_type",
     searchable: true,
   }),
   pagination({
-    container: '#pagination',
+    container: "#pagination",
   }),
 ]);
 
