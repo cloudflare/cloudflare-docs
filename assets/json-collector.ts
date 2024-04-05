ff6b88edd43aeeaa3f13020c6188f631d35d864c
Max Phillips <mphillips@cloudflare.com>
2024-04-05 15:31:47 -0400

    Update json-collector

--- assets/json-collector.ts
+++ assets/json-collector.ts
@@ -2,7 +2,6 @@ import * as get_started from "data/learning-paths/get-started.json";
 import * as optimize_site_speed from "data/learning-paths/optimize-site-speed.json";
 import * as application_security from "data/learning-paths/application-security.json";
 import * as replace_vpn from "data/learning-paths/replace-vpn.json";
-import * as dns_filtering from "data/learning-paths/dns-filtering.json";
 import * as load_balancing from "data/learning-paths/load-balancing.json";
 import * as workers from "data/learning-paths/workers.json";
 import * as prevent_ddos_attacks from "data/learning-paths/prevent-ddos-attacks.json";
@@ -18,7 +17,6 @@ let learning_paths = [
   workers,
   get_started,
   replace_vpn,
-  dns_filtering,
   optimize_site_speed,
   application_security,
   cybersafe,
