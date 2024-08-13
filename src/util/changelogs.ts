4b3d743b67d411942d540e54dbe90269803d392e
Kian Newman-Hazel <kian@cloudflare.com>
2024-08-13 19:56:56 +0100

    [Starlight] Add Wrangler changelogs

--- src/util/changelogs.ts
+++ src/util/changelogs.ts
@@ -31,24 +31,6 @@ export async function getChangelogs() {
 export async function getWranglerChangelog(): Promise<
 	CollectionEntry<"changelogs">
 > {
-	return {
-		// @ts-expect-error id is a union of on-disk YAML files but we're adding this one dynamically
-		id: "wrangler",
-		collection: "changelogs",
-		data: {
-			link: "/workers/platform/changelog/wrangler/",
-			productName: "wrangler",
-			productLink: "/workers/wrangler/",
-			productArea: "Developer platform",
-			productAreaLink: "/workers/platform/changelog/platform/",
-			entries: [
-				{
-					publish_date: "01-01-2024",
-					description: "placeholder due to api rate limits",
-				},
-			],
-		},
-	};
 	const response = await fetch(
 		"https://api.github.com/repos/cloudflare/workers-sdk/releases",
 	);
@@ -84,6 +66,7 @@ export async function getWranglerChangelog(): Promise<
 				return {
 					publish_date: release.published_at.toISOString().substring(0, 10),
 					title: release.name.split("@")[1],
+					link: `https://github.com/cloudflare/workers-sdk/releases/tag/wrangler%40${release.name.split("@")[1]}`,
 					description: release.body,
 				};
 			}),
