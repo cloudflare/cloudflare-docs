d5d04561f73091d490059043ac30541450fa4925
Kian Newman-Hazel <kian@cloudflare.com>
2024-08-13 19:46:44 +0100

    [Starlight] Add config and misc files

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+import starlightPlugin from "@astrojs/starlight-tailwind";
+
+const accent = { 200: "#F6821F", 600: "#F6821F", 900: "#F0F0F0" };
+const gray = {
+	100: "#f6f6f6",
+	200: "#eeeeee",
+	300: "#c2c2c2",
+	400: "#8b8b8b",
+	500: "#585858",
+	700: "#383838",
+	800: "#272727",
+	900: "#181818",
+};
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: { accent, gray },
+		},
+	},
+	plugins: [starlightPlugin()],
+};
