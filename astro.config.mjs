1118d7cd4d254ac88cf4f6f5f993ac16ec7e8647
kodster28 <kody@cloudflare.com>
2024-08-19 17:33:05 -0500

    [Chore] More analytics cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -82,10 +82,11 @@ export default defineConfig({
 			},
 			favicon: "/favicon.png",
 			head: [
-        {
-          tag: "script",
-          content: "<!-- Google Tag Manager -->(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NDGPDFZ');<!-- End Google Tag Manager -->"
-        },
+				{
+					tag: "script",
+					content:
+						"(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NDGPDFZ');",
+				},
 				{
 					tag: "meta",
 					attrs: {
@@ -128,7 +129,7 @@ export default defineConfig({
 				PageTitle: "./src/components/overrides/PageTitle.astro",
 				Pagination: "./src/components/overrides/Pagination.astro",
 				SocialIcons: "./src/components/overrides/SocialIcons.astro",
-        SkipLink: "./src/components/overrides/SkipLink.astro",
+				SkipLink: "./src/components/overrides/SkipLink.astro",
 			},
 			sidebar: await autogenSections(),
 			customCss: [
