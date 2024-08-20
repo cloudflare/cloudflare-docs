8cfa4c66e4c4beb7a09a92a3934a0346379b05bc
Kian <kian@kian.org.uk>
2024-08-20 14:31:43 +0100

    [Docs Site] Render individual_page entries in RSS feeds (#16301)

--- src/util/description.ts
+++ src/util/description.ts
@@ -1,9 +1,6 @@
-import { experimental_AstroContainer } from "astro/container";
-import { getContainerRenderer } from "@astrojs/mdx";
-import { loadRenderers } from "astro:container";
 import type { CollectionEntry } from "astro:content";
 import { parse } from "node-html-parser";
-
+import { entryToString } from "./container"
 /*
     1. If there is a `description` property in the frontmatter, return that.
     2. If there is a `<p>...</p>` element in the HTML, return that.
@@ -12,20 +9,9 @@ import { parse } from "node-html-parser";
 export async function getPageDescription(entry: CollectionEntry<"docs">) {
 	if (entry.data.description) return entry.data.description;
 
-	if (!entry.render) {
-		return undefined;
-	}
+	const html = await entryToString(entry);
 
-	const renderers = await loadRenderers([getContainerRenderer()]);
-	const container = await experimental_AstroContainer.create({
-		renderers,
-	});
-
-	const { Content } = await entry.render();
-
-	const html = await container.renderToString(Content, {
-		params: { slug: entry.slug },
-	});
+	if (!html) return undefined;
 
 	const dom = parse(html);
 	const description = dom.querySelector(":root > p");
