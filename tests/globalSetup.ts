1c0805d1f5719cc026b1c23999cafb46181ecc1e
Greg Brimble <gbrimble@cloudflare.com>
2024-10-15 10:26:12 -0400

    omg Puppeteer tests (#17523)

---
+++ tests/globalSetup.ts
@@ -0,0 +1,19 @@
+import type { GlobalSetupContext } from "vitest/node";
+import puppeteer, { Browser } from "puppeteer";
+
+let browser: Browser;
+
+export default async function setup({ provide }: GlobalSetupContext) {
+	browser = await puppeteer.launch();
+	provide("browserWSEndpoint", browser.wsEndpoint());
+}
+
+export async function teardown() {
+	await browser.close();
+}
+
+declare module "vitest" {
+	export interface ProvidedContext {
+		browserWSEndpoint: string;
+	}
+}
