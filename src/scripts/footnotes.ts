f0be236c146a1be415bf614bf1afbc6c067668cf
ranbel <101146722+ranbel@users.noreply.github.com>
2024-10-22 12:25:14 -0400

    [ZT] Update minimum MTU (#17522)
    * Update minimum MTU
    
    * move content to footnote
    
    * Rebase and fix multiple footnote blocks
    
    ---------
    
    Co-authored-by: Kian Newman-Hazel <kian@cloudflare.com>

--- src/scripts/footnotes.ts
+++ src/scripts/footnotes.ts
@@ -1,26 +1,28 @@
 import { addTooltip } from "~/util/tippy";
 
-const footnotes = document.querySelector("section.footnotes");
+const footnotes = document.querySelectorAll("section.footnotes");
 
 if (footnotes) {
-	const notes = footnotes.querySelectorAll("li");
+	for (const section of footnotes) {
+		const notes = section.querySelectorAll("li");
 
-	for (const note of notes) {
-		const content = note.querySelector("p") as HTMLParagraphElement;
+		for (const note of notes) {
+			const content = note.querySelector("p") as HTMLParagraphElement;
 
-		const fnrefs = document.querySelectorAll<HTMLAnchorElement>(
-			`a[id^='${note.id.replace("fn", "fnref")}']`,
-		);
+			const fnrefs = document.querySelectorAll<HTMLAnchorElement>(
+				`a[id^='${note.id.replace("fn", "fnref")}']`,
+			);
 
-		for (const fnref of fnrefs) {
-			addTooltip(fnref, content.innerHTML);
+			for (const fnref of fnrefs) {
+				addTooltip(fnref, content.innerHTML);
 
-			fnref.classList.add("footnote");
+				fnref.classList.add("footnote");
 
-			fnref.setAttribute("tabindex", "0");
-			fnref.removeAttribute("href");
+				fnref.setAttribute("tabindex", "0");
+				fnref.removeAttribute("href");
+			}
 		}
-	}
 
-	footnotes.remove();
+		section.remove();
+	}
 }
