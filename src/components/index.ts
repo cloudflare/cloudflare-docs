5882dc01d41e589ed2b629ee885ea0564e6a6de8
Kian <kian@kian.org.uk>
2024-10-23 11:33:57 +0100

    [Docs Site] Add Flex component for responsive image layouts (#17466)

--- src/components/index.ts
+++ src/components/index.ts
@@ -15,6 +15,7 @@ export { default as Example } from "./Example.astro";
 export { default as ExternalResources } from "./ExternalResources.astro";
 export { default as Feature } from "./Feature.astro";
 export { default as FeatureTable } from "./FeatureTable.astro";
+export { default as Flex } from "./Flex.astro";
 export { default as Glossary } from "./Glossary.astro";
 export { default as GlossaryDefinition } from "./GlossaryDefinition.astro";
 export { default as GlossaryTooltip } from "./GlossaryTooltip.astro";
