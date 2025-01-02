7205cd42c67f8eeea40eb528d2d9db7a9ee56baf
Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
2025-01-02 17:57:16 +0000

    Catalog: Add tooltip with name & data type

--- src/components/FieldCatalog.jsx
+++ src/components/FieldCatalog.jsx
@@ -156,10 +156,13 @@ const FieldCatalog = ({ fields }) => {
 						<a
 							key={field.name}
 							className="p-3 border-gray-200 dark:border-gray-700 border-solid border rounded-md lg:w-[48%] w-full block !text-inherit no-underline self-stretch hover:bg-gray-50 dark:hover:bg-black mb-3"
-							href={`/ruleset-engine/rules-language/fields/reference/${field.name}`}
+							href={`/ruleset-engine/rules-language/fields/reference/${field.name}/`}
 						>
 							<div className="-mb-1 flex items-center">
-								<span className="font-semibold text-lg text-ellipsis overflow-hidden whitespace-nowrap">
+								<span
+									className="font-semibold text-lg text-ellipsis overflow-hidden whitespace-nowrap"
+									title={`${field.name}: ${field.data_type}`}
+								>
 									{field.name}
 								</span>
 							</div>
