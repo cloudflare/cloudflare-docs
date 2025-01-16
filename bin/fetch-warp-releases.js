8d837270253ee0a13e0080b486156a676be873ac
Kian Newman-Hazel <kian@cloudflare.com>
2025-01-16 15:45:20 +0000

    backfill releases

--- bin/fetch-warp-releases.js
+++ bin/fetch-warp-releases.js
@@ -1,7 +1,15 @@
 import fs from "fs";
 import YAML from "yaml";
+import { marked } from "marked";
 
 const tracks = ["windows/ga", "windows/beta", "macos/ga", "macos/beta"];
+
+const linesToRemove = [
+	"For related Cloudflare for Teams documentation please see: https://developers.cloudflare.com/cloudflare-one/connections/connect-devices/warp",
+	"For Zero Trust documentation please see: https://developers.cloudflare.com/cloudflare-one/connections/connect-devices/warp",
+	"For related Consumer documentation please see: https://developers.cloudflare.com/warp-client/",
+	"For Consumer documentation please see: https://developers.cloudflare.com/warp-client/",
+];
 
 for (const track of tracks) {
 	fetch(`https://downloads.cloudflareclient.com/v1/update/json/${track}`)
@@ -17,15 +25,35 @@ for (const track of tracks) {
 
 				console.log(`Saving ${track} ${item.version}.`);
 
-				const platformName = data.platformName;
-				const releaseNotes = item.releaseNotes.replace(/\r\n/g, "\n");
+				let markdown = item.releaseNotes;
+
+				markdown.replace(/\r\n/g, "\n");
+
+				for (const line of linesToRemove) {
+					markdown = markdown.replace(line, "");
+				}
+
+				markdown = markdown.trim();
+
+				const tokens = marked.lexer(markdown);
+
+				marked.walkTokens(tokens, (token) => {
+					if (token.type === "heading") {
+						token.type = "strong";
+						token.raw = `**${token.text}**\n`;
+
+						delete token.depth;
+					}
+				});
+
+				const releaseNotes = tokens.reduce((s, t) => s + t.raw, "");
 
 				fs.writeFileSync(
 					`./src/content/warp-releases/${track}/${item.version}.yaml`,
 					YAML.stringify({
 						...item,
 						releaseNotes,
-						platformName,
+						platformName: data.platformName,
 					}),
 					"utf-8",
 				);
