3d343cece66dd88f789ea8babf174f0cd3df8d62
Kian Newman-Hazel <kian@cloudflare.com>
2025-01-21 21:18:15 +0000

    [Docs Site] Restore analytics events for various elements

--- src/scripts/analytics.ts
+++ src/scripts/analytics.ts
@@ -1,37 +1,9 @@
-const links = document.querySelectorAll<HTMLAnchorElement>("a");
+import { registerLinks } from "./analytics/links";
+import { registerTabs } from "./analytics/tabs";
+import { registerDetails } from "./analytics/details";
+import { registerCopyButtons } from "./analytics/codeblocks";
 
-function $zarazLinkEvent(type: string, link: HTMLAnchorElement) {
-	// @ts-expect-error TODO: type zaraz
-	zaraz.track(type, { href: link.href, hostname: link.hostname });
-}
-function registerLinkAnalytics() {
-	if (!links || links.length === 0) {
-		return;
-	}
-	for (const link of links) {
-		if (!link.href) {
-			continue;
-		}
-		const linkURL = new URL(link.href);
-		const cfSubdomainRegex = new RegExp(`^[^.]+?\\.cloudflare\\.com`);
-		if (linkURL.hostname !== "developers.cloudflare.com") {
-			if (
-				linkURL.hostname === "workers.cloudflare.com" &&
-				linkURL.pathname.startsWith("/playground#")
-			) {
-				link.addEventListener("click", () => {
-					$zarazLinkEvent("playground link click", link);
-				});
-			} else if (cfSubdomainRegex.test(linkURL.hostname)) {
-				link.addEventListener("click", () => {
-					$zarazLinkEvent("Cross Domain Click", link);
-				});
-			} else {
-				link.addEventListener("click", () => {
-					$zarazLinkEvent("external link click", link);
-				});
-			}
-		}
-	}
-}
-registerLinkAnalytics();
+registerLinks();
+registerTabs();
+registerDetails();
+registerCopyButtons();
