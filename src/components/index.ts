dbcc83b1ab6d6ef0c68e52fa30fdce62ba5f7fb2
Kian Newman-Hazel <kian@cloudflare.com>
2025-03-03 09:01:15 +0000

    [Docs Site] Add APIRequest and CURL components

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,9 +6,11 @@ export { PackageManagers } from "starlight-package-managers";
 export { Icon as AstroIcon } from "astro-icon/components";
 // Custom components
 export { default as AnchorHeading } from "./AnchorHeading.astro";
+export { default as APIRequest } from "./APIRequest.astro";
 export { default as AvailableNotifications } from "./AvailableNotifications.astro";
 export { default as CompatibilityFlag } from "./CompatibilityFlag.astro";
 export { default as CompatibilityFlags } from "./CompatibilityFlags.astro";
+export { default as CURL } from "./CURL.astro";
 export { default as Description } from "./Description.astro";
 export { default as Details } from "./Details.astro";
 export { default as DirectoryListing } from "./DirectoryListing.astro";
