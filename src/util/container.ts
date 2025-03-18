45d879d30418d8c15f747543deafbf874c3e222d
Kian Newman-Hazel <kian@cloudflare.com>
2025-03-18 15:18:36 +0000

    [Docs Site] Add React to Container API

--- src/util/container.ts
+++ src/util/container.ts
@@ -1,5 +1,6 @@
 import { experimental_AstroContainer } from "astro/container";
-import { getContainerRenderer } from "@astrojs/mdx";
+import reactRenderer from "@astrojs/react/server.js";
+import mdxRenderer from "@astrojs/mdx/server.js";
 import { loadRenderers } from "astro:container";
 import { render, type CollectionEntry } from "astro:content";
 import type { AstroComponentFactory } from "astro/runtime/server/index.js";
@@ -12,9 +13,14 @@ export async function entryToString(
 		return entry.rendered.html;
 	}
 
-	const renderers = await loadRenderers([getContainerRenderer()]);
-	const container = await experimental_AstroContainer.create({
-		renderers,
+	const container = await experimental_AstroContainer.create({});
+	container.addServerRenderer({
+		name: "astro:jsx",
+		renderer: mdxRenderer,
+	});
+	container.addServerRenderer({
+		name: "@astrojs/react",
+		renderer: reactRenderer,
 	});
 
 	const { Content } = await render(entry);
