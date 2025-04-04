2e83ea550bca86f6707827884c36811cc30d0cad
Kian Newman-Hazel <kian@cloudflare.com>
2025-04-04 20:53:15 +0100

    [Docs Site] Add workflow to update api-schemas pinned commit

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,12 +1,14 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
 import type { OpenAPI } from "openapi-types";
 
+const COMMIT = "f6c9d752f31f2e9dea3a9659fefab1b97b6042e9";
+
 let schema: OpenAPI.Document | undefined;
 
 export const getSchema = async () => {
 	if (!schema) {
 		const response = await fetch(
-			"https://gh-code.developers.cloudflare.com/cloudflare/api-schemas/f6c9d752f31f2e9dea3a9659fefab1b97b6042e9/openapi.json",
+			`https://gh-code.developers.cloudflare.com/cloudflare/api-schemas/${COMMIT}/openapi.json`,
 		);
 		const obj = await response.json();
 
