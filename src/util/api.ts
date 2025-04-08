25d02a5f5285abf3f4c096c44cfbd29047e49b0b
Kody Jackson <kody@cloudflare.com>
2025-04-08 10:05:59 -0500

    Update src/util/api.ts

--- src/util/api.ts
+++ src/util/api.ts
@@ -3,7 +3,6 @@ import type { OpenAPI } from "openapi-types";
 
 const COMMIT = "573bd99b9a6ba6d5e17f13099f9db6606ff92bc9";
 
-
 let schema: OpenAPI.Document | undefined;
 
 export const getSchema = async () => {
