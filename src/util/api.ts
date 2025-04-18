3df4064a539d56893616a89cea05fbfd1d088893
Kody Jackson <kody@cloudflare.com>
2025-04-18 10:38:57 -0500

    Update src/util/api.ts

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,7 +1,7 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
 import type { OpenAPI } from "openapi-types";
 
-const COMMIT = "e230d6ba9f7171cbe73e5574b24c00fd2e9bca05";
+const COMMIT = "e230d6ba9f7171cbe73e5574b24c00fd2e9bca05"; 
 let schema: OpenAPI.Document | undefined;
 
 export const getSchema = async () => {
