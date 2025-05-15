8ce121f2471cb47669ee70e0ef8bc362d2d70d48
Kian Newman-Hazel <kian@cloudflare.com>
2025-05-15 19:40:37 +0100

    [Docs Site] Use ClipboardItem for async fetch in CopyPageButton

--- src/components/CopyPageButton.tsx
+++ src/components/CopyPageButton.tsx
@@ -50,14 +50,15 @@ export default function CopyPageButton() {
 	const handleCopyMarkdown = async () => {
 		const markdownUrl = new URL("index.md", window.location.href).toString();
 		try {
-			const response = await fetch(markdownUrl);
+			const clipboardItem = new ClipboardItem({
+				["text/plain"]: fetch(markdownUrl)
+					.then((r) => r.text())
+					.catch((e) => {
+						throw new Error(`Received ${e.message} for ${markdownUrl}`);
+					}),
+			});
 
-			if (!response.ok) {
-				throw new Error(`Received ${response.status} on ${response.url}`);
-			}
-
-			const markdown = await response.text();
-			await navigator.clipboard.writeText(markdown);
+			await navigator.clipboard.write([clipboardItem]);
 			track("clicked copy page button", {
 				value: "copy markdown",
 			});
