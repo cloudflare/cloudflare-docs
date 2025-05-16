1fb914b82a28a5d4e0e5274efd85d786c2ac4412
Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
2025-05-16 10:10:45 +0100

    Trigger build

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,7 +1,7 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
 import type { OpenAPI } from "openapi-types";
 
-const COMMIT = "f3df4fd1316367911e973536e8eb7b6995ab76b3";
+const COMMIT = "f3df4fd1316367911e973536e8eb7b6995ab76b3"; 
 let schema: OpenAPI.Document | undefined;
 
 export const getSchema = async () => {
