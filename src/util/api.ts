31f2ff4243b947644a28d44d698a10c9bed75fed
Kian <kian@kian.org.uk>
2025-05-23 11:24:05 +0100

    Update src/util/api.ts

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,7 +1,7 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
 import type { OpenAPI } from "openapi-types";
 
-const COMMIT = "ca0ad2b8537928266a0a56e4a17d469a892fdbfc";
+const COMMIT = "ca0ad2b8537928266a0a56e4a17d469a892fdbfc"; 
 let schema: OpenAPI.Document | undefined;
 
 export const getSchema = async () => {
