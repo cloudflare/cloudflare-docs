51d023ebc2abc9c6b81f63d8782fa7b930980f56
Pedro Sousa <680496+pedrosousa@users.noreply.github.com>
2025-05-26 08:25:25 +0100

    Undo change

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,7 +1,7 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
 import type { OpenAPI } from "openapi-types";
 
-const COMMIT = "eb4b9ef079c67bb22c7ce2d6af5f353ce10c5057"; 
+const COMMIT = "eb4b9ef079c67bb22c7ce2d6af5f353ce10c5057";
 let schema: OpenAPI.Document | undefined;
 
 export const getSchema = async () => {
