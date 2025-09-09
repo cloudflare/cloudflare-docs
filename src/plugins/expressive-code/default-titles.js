ff9653963b317a3349df2c344428d9d71a87092e
kodster28 <kody@cloudflare.com>
2025-09-09 15:25:56 -0500

    [Chore] Add default language title if none set

--- src/plugins/expressive-code/default-titles.js
+++ src/plugins/expressive-code/default-titles.js
@@ -10,6 +10,30 @@ export default () => {
 						context.codeBlock.props.title ??= "PowerShell";
 						break;
 					}
+					case "javascript": {
+						context.codeBlock.props.title ??= "index.js";
+						break;
+					}
+					case "js": {
+						context.codeBlock.props.title ??= "index.js";
+						break;
+					}
+					case "py": {
+						context.codeBlock.props.title ??= "index.py";
+						break;
+					}
+					case "python": {
+						context.codeBlock.props.title ??= "index.py";
+						break;
+					}
+					case "typescript": {
+						context.codeBlock.props.title ??= "index.ts";
+						break;
+					}
+					case "ts": {
+						context.codeBlock.props.title ??= "index.ts";
+						break;
+					}
 					default: {
 						return;
 					}
