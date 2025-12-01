e241ee0ce14fac1cc39b5020499c29d8282ee62b
kodster28 <kody@cloudflare.com>
2025-12-01 09:49:46 -0600

    more cleanup

--- src/components/index.ts
+++ src/components/index.ts
@@ -53,7 +53,6 @@ export { default as Render } from "./Render.astro";
 export { default as ResourcesBySelector } from "./ResourcesBySelector.astro";
 export { default as RSSButton } from "./RSSButton.astro";
 export { default as RuleID } from "./RuleID.astro";
-export { default as SpotlightAuthorDetails } from "./SpotlightAuthorDetails.astro";
 export { default as Stream } from "./Stream.astro";
 export { default as TagsUsage } from "./TagsUsage.astro";
 export { default as TunnelCalculator } from "./TunnelCalculator.astro";
