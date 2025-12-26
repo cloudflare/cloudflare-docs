83c4b925ae8355cca841b203ad6c6a8e5f876b70
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-12-26 22:30:13 +0900

    feat: add flake.nix
    * fix: add docs for customising design system
    
    * fix: add slug
    
    * fix: sidebar order

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "This flakes provides devShell for cloudflare-docs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+            corepack
+          ];
+        };
+      }
+    );
+}
