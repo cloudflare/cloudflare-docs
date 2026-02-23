67f5555c7fdd741e78567a58b14bc667b5715f04
Matt Rothenberg <mrothenberg@cloudflare.com>
2026-02-23 12:05:19 -0500

    style: format JsonSchemaViewer

--- src/components/models/JsonSchemaViewer.tsx
+++ src/components/models/JsonSchemaViewer.tsx
@@ -1,22 +1,24 @@
-import { JsonSchemaViewer } from 'cf-json-schema-viz';
-import 'cf-json-schema-viz/styles.css';
+import { JsonSchemaViewer } from "cf-json-schema-viz";
+import "cf-json-schema-viz/styles.css";
 
 interface Props {
-  schema: object;
+	schema: object;
 }
 
 export default function JsonSchemaViewerWrapper({ schema }: Props) {
-  return (
-    <div style={{
-      border: '1px solid var(--sl-color-gray-6)',
-      borderRadius: '0.5rem',
-      backgroundColor: 'var(--sl-color-bg)',
-    }}>
-      <JsonSchemaViewer
-        schema={schema}
-        defaultExpandedDepth={2}
-        maxHeight={512}
-      />
-    </div>
-  );
+	return (
+		<div
+			style={{
+				border: "1px solid var(--sl-color-gray-6)",
+				borderRadius: "0.5rem",
+				backgroundColor: "var(--sl-color-bg)",
+			}}
+		>
+			<JsonSchemaViewer
+				schema={schema}
+				defaultExpandedDepth={2}
+				maxHeight={512}
+			/>
+		</div>
+	);
 }
