dba2430d26d03dda723897d7cdda0306be41399c
kodster28 <kody@cloudflare.com>
2026-03-19 14:37:56 -0500

    [chore] Bump wrangler and update test timeout

--- src/components/WranglerCLI.astro.test.ts
+++ src/components/WranglerCLI.astro.test.ts
@@ -18,8 +18,8 @@ describe("WranglerCLI", () => {
 					command: "deploy",
 				},
 			}),
-		).resolves.toContain("pnpm wrangler deploy");
-	});
+		).resolves.toContain("pnpm wrangler deploy")
+	}, 10_000);
 
 	test("errors with no props", async () => {
 		await expect(renderWithOptions()).rejects
