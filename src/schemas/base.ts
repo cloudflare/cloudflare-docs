a96096ed2263b1241b760990cda985e8e3457089
kodster28 <kody@cloudflare.com>
2026-03-26 15:43:11 -0500

    Remove destructuring

--- src/schemas/base.ts
+++ src/schemas/base.ts
@@ -3,7 +3,7 @@ import { reference, type SchemaContext } from "astro:content";
 
 import { sidebar } from "./types/sidebar";
 
-export const baseSchema = (_: SchemaContext) =>
+export const baseSchema = (context: SchemaContext) =>
 	z.object({
 		pcx_content_type: z
 			.string()
