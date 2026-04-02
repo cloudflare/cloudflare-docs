1105e997feddb094dd8794d06a378fc9cebb8234
joslyn-cf <133819699+joslyn-cf@users.noreply.github.com>
2026-04-02 17:54:36 +0100

    Update tags.ts
    adding missing punctuation

--- src/schemas/tags.ts
+++ src/schemas/tags.ts
@@ -99,7 +99,7 @@ const protocols: Array<Tag> = [
 	{ label: "mTLS" },
 	{ label: "NetFlow" },
 	{ label: "MQTT" },
-	{ label: "OIDC", variants ["OpenID Connect", "oidc"]},
+	{ label: "OIDC", variants: ["OpenID Connect", "oidc"]},
 	{ label: "Post-quantum", variants: ["pqc", "post-quantum cryptography"] },
 	{ label: "QUIC" },
 	{ label: "RDP" },
@@ -141,7 +141,7 @@ const use_cases: Array<Tag> = [
 	{ label: "Migration" },
 	{ label: "MCP" },
 	{ label: "Playback" },
-	{ label: "Posture", variants: ["security posture", "device posture"] }
+	{ label: "Posture", variants: ["security posture", "device posture"] },
 	{ label: "Prisma ORM" },
 	{ label: "Privacy" },
 	{ label: "Private networks" },
