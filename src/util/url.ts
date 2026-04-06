d39f7c9f4a4a9196057220eabfd5e7765a1f84aa
mvm <vance@cloudflare.com>
2026-04-06 15:27:23 -0500

    revert formatting check error

--- src/util/url.ts
+++ src/util/url.ts
@@ -1,5 +1,5 @@
 export function setSearchParams(params: URLSearchParams) {
-  if (params.size === 0) {
+	if (params.size === 0) {
 		history.pushState(null, "", window.location.pathname);
 		return;
 	}
