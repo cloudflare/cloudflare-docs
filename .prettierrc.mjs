4265697bbec9ec1e9a5b4ef3a972912bf2f2fa29
mvm <vance@cloudflare.com>
2026-04-11 17:56:47 -0500

    custom prettier plugin for inline mdx components

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,13 +1,24 @@
 // @ts-check
 /** @type {import("prettier").Config} */
 export default {
-	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+	plugins: [
+		"prettier-plugin-astro",
+		"prettier-plugin-tailwindcss",
+		"./plugins/prettier-plugin-mdx-inline/index.mjs",
+	],
 	useTabs: true,
 	overrides: [
 		{
 			files: "*.astro",
 			options: {
 				parser: "astro",
+			},
+		},
+		{
+			files: "*.mdx",
+			options: {
+				parser: "mdx-inline",
+				mdxInlineElements: "code,GlossaryTooltip",
 			},
 		},
 	],
