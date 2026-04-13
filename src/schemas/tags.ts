6eec27bb57761c667ae46b5aeddfcc83f35bfd46
Ranbel Sun <ranbel@cloudflare.com>
2026-04-13 13:15:16 -0400

    remove Mesh from tags

--- src/schemas/tags.ts
+++ src/schemas/tags.ts
@@ -149,7 +149,6 @@ const use_cases: Array<Tag> = [
 	{ label: "LLM", variants: ["llms"] },
 	{ label: "Localization" },
 	{ label: "Logging" },
-	{ label: "Mesh", variants: ["Cloudflare Mesh"] },
 	{ label: "Middleware" },
 	{ label: "Migration" },
 	{ label: "MCP" },
