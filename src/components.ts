21a39d72ef1f3287fa0d8fa8a1328e8b4c7ec4f9
mmalden <179233968+mia303@users.noreply.github.com>
2026-08-03 16:33:53 -0400

    [Artifacts] Add changelog

--- src/components.ts
+++ src/components.ts
@@ -70,9 +70,6 @@ export { default as AiSearchNamespacesDiagram } from "./components/AiSearchNames
 export { default as AiSearchOverviewDiagram } from "./components/AiSearchOverviewDiagram.astro";
 export { default as AiSearchQueryingDiagram } from "./components/AiSearchQueryingDiagram.astro";
 export { default as AiSearchVoiceDiagram } from "./components/AiSearchVoiceDiagram.astro";
-export { default as ArtifactsCIPipelineDiagram } from "./components/ArtifactsCIPipelineDiagram.astro";
-export { default as ArtifactsCIWorkflowDiagram } from "./components/ArtifactsCIWorkflowDiagram.astro";
-export { default as ArtifactsPlatformSharedCIDiagram } from "./components/ArtifactsPlatformSharedCIDiagram.astro";
 export { default as AutoconfigDiagram } from "./components/cf/AutoconfigDiagram.astro";
 export { default as WorkersArchitectureDiagram } from "./components/cf/WorkersArchitectureDiagram.astro";
 export { default as WorkersIsolateDiagram } from "./components/cf/WorkersIsolateDiagram.astro";
