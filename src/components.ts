80397af0ae019ed41c6403bf8d4e6426bb3bab12
korinne <korinne@cloudflare.com>
2026-09-21 16:52:04 -0700

    [Realtime] Rewrite overview page

--- src/components.ts
+++ src/components.ts
@@ -1,6 +1,9 @@
 export { Aside } from "./components/ui/aside";
 export { Card } from "./components/ui/card";
 export { CardGrid } from "./components/ui/card-grid";
+export { default as GuideCard } from "./components/GuideCard.astro";
+export { default as GuideCardGrid } from "./components/GuideCardGrid.astro";
+export { default as RealtimeProductChooser } from "./components/realtime/RealtimeProductChooser.astro";
 export { PackageManagers } from "./components/ui/package-managers";
 export { Step, Steps } from "./components/ui/steps";
 export { TabItem, Tabs } from "./components/ui/tabs";
